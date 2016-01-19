0d69006415acbb3b790f42c4817035ea788dbc54
Ryan Foster <ryan@rynamic.com>
2016-01-19 14:38:09 -0800

    new license

--- StaticResourceSources/ManageHousehold/ManageHousehold.js
+++ StaticResourceSources/ManageHousehold/ManageHousehold.js
@@ -1,6 +1,38 @@
-// Written by Ryan Foster, copyright (c) 2011 Groundwire
-// This program is released under the GNU Affero General Public License, Version 3. http://www.gnu.org/licenses/
-// 04/01/2012 James Melville Changes made throughout to support NPSP HH
+/*
+    Originally written in 2011 by Ryan Foster
+
+    04/01/2012 James Melville Changes made throughout to support NPSP HH
+
+    Updated January 2016 by Ryan Foster and relicensed:
+
+    Copyright (c) 2016, Salesforce.org
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Salesforce.org nor the names of
+      its contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
 
 (function($) {
   'use strict';
