9933cd0a2a8096b458d8d01e3572610f216ff01e
David Habib <dhabib@salesforce.com>
2016-05-25 10:13:40 -0700

    convert tabs to spaces

--- src/aura/HH_AddressMgr/HH_AddressMgrController.js
+++ src/aura/HH_AddressMgr/HH_AddressMgrController.js
@@ -1,5 +1,5 @@
 ({
-	doInit : function(component, event, helper) {
+    doInit : function(component, event, helper) {
         var addr = {
             Mailing_Street__c : null,
             Mailing_Street2__c : null,
@@ -9,12 +9,12 @@
             Mailing_Country__c : null
         };
         component.set('v.addrNew', addr);
-	},
+    },
 
     onChangeListAddr : function(component, event, helper) {
-		var listAddr = component.get('v.listAddr'); 
+        var listAddr = component.get('v.listAddr');
         if (listAddr.length > 0) {
-            
+
             // find the default address
             var iAddrDefault = 0;
             for (var i = 0; i < listAddr.length; i++) {
@@ -24,32 +24,32 @@
                 }
             }
             component.set('v.addrDefault', listAddr[iAddrDefault]);
-        }            
-	},
-    
-	openChangeAddress : function(component, event, helper) {
+        }
+    },
+
+    openChangeAddress : function(component, event, helper) {
         component.set('v.showChangeAddressPopup', true);
-	},
+    },
 
-	cancelChangeAddress : function(component, event, helper) {
+    cancelChangeAddress : function(component, event, helper) {
         component.set('v.showChangeAddressPopup', false);
-	},
+    },
 
-	saveChangeAddress : function(component, event, helper) {
+    saveChangeAddress : function(component, event, helper) {
         helper.setDefaultAddress(component);
         component.set('v.showChangeAddressPopup', false);
-	},
+    },
 
-	onPressAddrTile : function(component, event, helper) {
+    onPressAddrTile : function(component, event, helper) {
         var iAddr = Number(event.currentTarget.getAttribute('data-iAddr'));
         component.set('v.iAddrSelected', iAddr);
         component.set('v.listAddr', component.get('v.listAddr'));
         component.set('v.showChangeAddressPopup', true);
-	},
+    },
 
-	toggleAddrSection : function(component, event, helper) {
+    toggleAddrSection : function(component, event, helper) {
         var isOpen = component.get('v.isExistingAddrSectionOpen');
         component.set('v.isExistingAddrSectionOpen', !isOpen);
-	},
+    },
 
 })
