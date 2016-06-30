aaecc4ce7c0017449d1283f54c72d27e95893b4c
David Habib <dhabib@salesforce.com>
2016-06-30 07:30:49 -0700

    more namespace fixes

--- src/aura/HH_AddressMgr/HH_AddressMgrController.js
+++ src/aura/HH_AddressMgr/HH_AddressMgrController.js
@@ -36,8 +36,6 @@
     onPressAddrTile : function(component, event /* , helper */) {
         var iAddr = Number(event.currentTarget.getAttribute('data-iAddr'));
         component.set('v.iAddrSelected', iAddr);
-        //component.set('v.listAddr', component.get('v.listAddr'));
-        //component.set('v.showChangeAddressPopup', true);
     },
 
     toggleAddrSection : function(component /* , event, helper */) {
