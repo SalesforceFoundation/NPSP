bc0b72547cef29102b88c77cd35fdeee070ee941
Jason Lantz <jlantz@salesforce.com>
2016-12-08 07:15:58 -0600

    - Implemented custom UpdateAdminProfile that sets record type   visibilities - Remove test_results.json processing from CircleCI build since we're   not using the debugging test runner at the moment

---
+++ tasks/salesforce.py
@@ -0,0 +1,42 @@
+import os
+from cumulusci.tasks.salesforce import UpdateAdminProfile as BaseUpdateAdminProfile
+from cumulusci.utils import findReplace
+from cumulusci.utils import findReplaceRegex
+
+rt_visibility_template = """
+<recordTypeVisibilities>
+    <default>{}</default>
+    <personAccountDefault>true</personAccountDefault>
+    <recordType>{}</recordType>
+    <visible>true</visible>
+</recordTypeVisibilities>
+"""
+
+class UpdateAdminProfile(BaseUpdateAdminProfile):
+        
+    def _process_metadata(self):
+        super(UpdateAdminProfile, self)._process_metadata()
+        
+        # Strip record type visibilities
+        findReplaceRegex(
+            '<recordTypeVisibilities>([^\$]+)</recordTypeVisibilities>',
+            '',
+            os.path.join(self.tempdir, 'profiles'),
+            'Admin.profile'
+        )
+        
+        # Set record type visibilities
+        self._set_record_type('Account.HH_Account', 'false')
+        self._set_record_type('Account.Organization', 'true')
+        self._set_record_type('Opportunity.NPSP_Default', 'true')
+
+        self.logger.info(open(os.path.join(self.tempdir, 'profiles', 'Admin.profile'), 'r').read())
+
+    def _set_record_type(self, name, default):
+        rt = rt_visibility_template.format(default, name)
+        findReplace(
+            '<tabVisibilities>',
+            '{}<tabVisibilities>'.format(rt),
+            os.path.join(self.tempdir, 'profiles'),
+            'Admin.profile',
+        )
