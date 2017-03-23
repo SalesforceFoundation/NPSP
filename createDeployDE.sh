a4dc22ac347782117a86b91295d67527f06027c8
Robert Frankus <rfrankus@salesforce.com>
2017-03-23 09:32:45 -0700

    Add scripts

---
+++ createDeployDE.sh
@@ -0,0 +1,5 @@
+cci org default dev
+cci org list
+cci flow run dev_org
+cci task run update_admin_profile
+cci flow run test_data_dev_org
---
+++ createDeployScratch.sh
@@ -0,0 +1,5 @@
+export SFDX_ORG_CREATE_ARGS="-e prototype"
+
+cci org scratch dev scratch
+cci flow run dev_org --org scratch
+
