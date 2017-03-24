5554b9d1f0f5eff753efe0d5dcfe4ebbab457486
Robert Frankus <rfrankus@salesforce.com>
2017-03-24 10:16:45 -0700

    updated scripts

--- createDeployScratchJenkins.sh
+++ createDeployScratchJenkins.sh
@@ -5,4 +5,5 @@ export SFDX_ORG_CREATE_ARGS="-e prototype"
 
 cci org scratch dev scratch
 cci flow run dev_org --org scratch
-
+cci task run update_admin_profile --org scratch
+cci flow run test_data_dev_org --org scratch
