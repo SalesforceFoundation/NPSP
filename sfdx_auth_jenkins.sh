edb77fde388e0a53fa0e379315fabcf4a0cf2702
Robert Frankus <rfrankus@salesforce.com>
2017-03-24 09:50:45 -0700

    updated scripts

--- sfdx_auth_jenkins.sh
+++ sfdx_auth_jenkins.sh
@@ -1,2 +1,3 @@
 export $(dbus-launch)
 sfdx force:auth:jwt:grant -i 3MVG9nwRZX60mjQra5yKwxXiTWO_xUsEemUpLrgDCf_A7sBoMdzIgBY2HDi0QmmchchQLYwgpqkD_2hF6hZ4J -f ~/.ssh/server.key --setdefaultusername  -u rfrankus@sfdx-salesforce.org
+sfdx force:config:set defaultdevhubusername=rfrankus@sfdx-salesforce.org
