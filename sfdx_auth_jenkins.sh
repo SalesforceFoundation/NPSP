85bce124ba59c19825c2390c32df8a938afe6cdc
Robert Frankus <rfrankus@salesforce.com>
2017-03-24 09:02:34 -0700

    updated scripts

--- sfdx_auth_jenkins.sh
+++ sfdx_auth_jenkins.sh
@@ -1,1 +1,1 @@
-sfdx force:auth:jwt:grant -i   -f ~/.ssh/server.key --setdefaultusername  -u rfrankus@sfdx-salesforce.org
+sfdx force:auth:jwt:grant -i 3MVG9nwRZX60mjQra5yKwxXiTWO_xUsEemUpLrgDCf_A7sBoMdzIgBY2HDi0QmmchchQLYwgpqkD_2hF6hZ4J -f ~/.ssh/server.key --setdefaultusername  -u rfrankus@sfdx-salesforce.org
