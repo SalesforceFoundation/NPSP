9af05ffee2eb4acf861a0183097d12c625f315a7
Robert Frankus <rfrankus@salesforce.com>
2017-03-24 10:41:54 -0700

    updated scripts

--- createDeployScratchJenkins.sh
+++ createDeployScratchJenkins.sh
@@ -7,3 +7,12 @@ cci org scratch dev scratch
 cci flow run dev_org --org scratch
 cci task run update_admin_profile --org scratch
 cci flow run test_data_dev_org --org scratch
+
+echo "Get Org URL"
+sfdx force:org:open -r > scratch_session.txt 
+sleep 10s
+SCRATCHSESS=$(cat scratch_session.txt  | sed -n 1p | sed 's/.*sid=\(.*\)$/\1/')
+echo $SCRATCHSESS
+
+echo "Get Apex Test Results"
+python apexResultsRest.py -a Foundation -i $SCRATCHSESS
