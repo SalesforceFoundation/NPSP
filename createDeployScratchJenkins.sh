c444662b2021dff9158e25e1f927fccb6a7f075c
Robert Frankus <rfrankus@salesforce.com>
2017-03-29 14:50:24 -0700

    Updated scripts

--- createDeployScratchJenkins.sh
+++ createDeployScratchJenkins.sh
@@ -11,6 +11,11 @@ cci task run update_admin_profile --org scratch
 cci flow run test_data_dev_org --org scratch
 cci task run run_tests --org scratch
 
+sfdx force:org:list | tail -1 | sed 's/\(scratchorg[0-9]*@npsp-jenkinsworkspace.com\).*/\1/' > scratch_user.txt
+SCRATCHUSER=$(cat scratch_user.txt  | sed -n 1p | sed 's/\s*\(.*\)$/\1/')
+echo $SCRATCHUSER
+sfdx force:config:set defaultusername=$SCRATCHUSER
+
 echo "Get Org URL"
 sfdx force:org:open -r > scratch_session.txt 
 sleep 10s
@@ -18,4 +23,4 @@ SCRATCHSESS=$(cat scratch_session.txt  | sed -n 1p | sed 's/.*sid=\(.*\)$/\1/')
 echo $SCRATCHSESS
 
 echo "Get Apex Test Results"
-python apexResultsRest.py -a Foundation -i $SCRATCHSESS
+python apexResultsRest.py -a Foundation -i $SCRATCHSESSCRATCHUSER=$(cat scratch_user.txt  | sed -n 1p | sed 's/\s*\(.*\)$/\1/')S
