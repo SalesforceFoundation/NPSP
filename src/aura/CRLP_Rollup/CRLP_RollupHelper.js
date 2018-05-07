752b21d30239402af092752de81c6115b846c110
Randi Wilson <randi.wilson@salesforce.com>
2018-05-07 11:49:14 -0400

    Fixes for days back help text, changed the error text to reflect 10000 day limit, fixed days back null error handling, and fixed field to roll up being nulled when accessed from row action menu.

--- src/aura/CRLP_Rollup/CRLP_RollupHelper.js
+++ src/aura/CRLP_Rollup/CRLP_RollupHelper.js
@@ -160,9 +160,8 @@
             cmp.set("v.detailFields", newFields);
         }
 
-        //todo: is this necessary? pulled in my commit
         var currentMode = cmp.get("v.mode");
-        if (currentMode !== 'view') {
+        if (currentMode === 'create') {
             //reset detail field to null to prompt user selection
             cmp.set("v.activeRollup.detailField", null);
         }
@@ -843,12 +842,15 @@
                 if (errors && errors[0] && errors[0].message) {
                     msg = errors[0].message;
                 }
+                this.toggleSpinner(cmp, false);
+
                 if (currentMode === 'delete') {
                     this.showToast(cmp, 'error', cmp.get("v.labels.rollupDeleteFail"), msg);
                 } else {
                     this.showToast(cmp, 'error', cmp.get("v.labels.rollupSaveFail"), msg);
+                    cmp.set("v.mode", "edit");
+                    helper.changeMode(cmp);
                 }
-                this.toggleSpinner(cmp, false);
             }
         });
         if (currentMode === 'delete') {
@@ -1235,9 +1237,13 @@
             this.sendMessage(cmp, 'validateCmp');
         }
 
-        //description set separately since we have direct access to this cmp
+        //description and intValue set separately since we have direct access to these
         if (!activeRollup.description) {
             cmp.find("descriptionInput").showHelpMessageIfInvalid();
+            canSave = false;
+        }
+        if (!activeRollup.intValue && renderMap["integerDays"]) {
+            cmp.find("integerInput").showHelpMessageIfInvalid();
             canSave = false;
         }
 
