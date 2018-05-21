f4e9159c981b0e4dd877c5204c77ff2b3e0edcc1
Randi Wilson <randi.wilson@salesforce.com>
2018-05-21 11:51:13 -0400

    Alphabetized fields for filter rules

--- src/aura/CRLP_FilterGroup/CRLP_FilterGroupHelper.js
+++ src/aura/CRLP_FilterGroup/CRLP_FilterGroupHelper.js
@@ -424,12 +424,13 @@
     },
 
     /**
-     * @description: resets the list of filter rules based on the
+     * @description: resets the list of filter rule fields based on the selected object
      * @param object - selected object API name
      */
     resetFilterRuleFields: function(cmp, object) {
         var objectDetails = cmp.get("v.objectDetails");
-        cmp.set("v.filteredFields", objectDetails[object]);
+        var sortedFields = this.sortFields(objectDetails[object]);
+        cmp.set("v.filteredFields", sortedFields);
     },
 
     /**
@@ -535,6 +536,23 @@
             'message': message
         });
         sendMessage.fire();
+    },
+
+    /**
+     * @description Sort provided fields by the field labels
+     * @param fields - list of fields to sort
+     */
+    sortFields: function(fields){
+        fields.sort(function (a, b) {
+            if (a.label < b.label) {
+                return -1;
+            }
+            if (a.label > b.label) {
+                return 1;
+            }
+            return 0;
+        });
+        return fields;
     },
 
     /**
