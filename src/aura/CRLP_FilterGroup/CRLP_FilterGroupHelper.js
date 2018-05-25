1deb450aa073e6902222f35bb12726146f54015f
Beth Breisnes <bbreisnes@salesforce.com>
2018-05-24 23:41:09 -0700

    allow for null filter group value with equals or not equals operators only

--- src/aura/CRLP_FilterGroup/CRLP_FilterGroupHelper.js
+++ src/aura/CRLP_FilterGroup/CRLP_FilterGroupHelper.js
@@ -411,6 +411,13 @@
         } else {
             cmp.set("v.filterRuleFieldType", 'text');
         }
+
+        // Allow for null values ONLY on Equals and Not Equals operators
+        if (operator === 'Equals' || operator === 'Not_Equals') {
+            cmp.set("v.isValueRequired",false);
+        } else {
+            cmp.set("v.isValueRequired",true);
+        }
     },
 
     /**
