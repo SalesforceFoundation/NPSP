547da903181f036ab2797650532868b5df130105
Randi Wilson <randi.wilson@salesforce.com>
2018-06-04 19:41:06 +0300

    Removed last error console statements and refactored into properly displaying a load error.

--- src/aura/CRLP_Rollup/CRLP_RollupController.js
+++ src/aura/CRLP_Rollup/CRLP_RollupController.js
@@ -67,14 +67,11 @@
             }
             else if (state === "ERROR") {
                 var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message: " +
-                            errors[0].message);
-                    }
-                } else {
-                    console.log("Unknown error");
+                var msg = "Unknown error";
+                if (errors && errors[0] && errors[0].message) {
+                    msg = errors[0].message;
                 }
+                helper.showToast(cmp, 'error', cmp.get("v.labels.rollupLoadError"), msg);
             }
             helper.toggleSpinner(cmp, false);
         });
