f956857744cef8b0e2f1c2ee894a79acf4c35476
Martin De Leon <martin@nimacloud.com>
2018-06-24 17:29:19 -0300

    Removed option to stretch all columns.

--- StaticResourceSources/BGEResources/js/grid/app.js
+++ StaticResourceSources/BGEResources/js/grid/app.js
@@ -64,7 +64,6 @@
                 sortIndicator: true,
                 fillHandle: true,
                 autoWrapRow: true,
-                stretchH: 'all',
                 minSpareRows: 0,
                 width: $scope.tableWidth,
                 height: $scope.tableHeight,
@@ -224,7 +223,7 @@
         }
 
         function modifyColWidthHandler(width, col) {
-
+            console.log('COLUMN ', col);
             if(col === 0){
 
                 return 5;
