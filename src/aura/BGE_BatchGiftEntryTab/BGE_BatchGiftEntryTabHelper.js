a580ef46c609a30bb9cf5725ace73e29825bb68a
Beth Breisnes <bbreisnes@salesforce.com>
2018-12-12 14:36:21 -0800

    refactored out FLS check into separate method

---
+++ src/aura/BGE_BatchGiftEntryTab/BGE_BatchGiftEntryTabHelper.js
@@ -0,0 +1,109 @@
+({
+    /**
+     * @description: instantiates component. Only called when component is first loaded.
+     */
+    doInit: function(component, event, helper) {
+        var action = component.get('c.isOrgNamespaced');
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var isOrgNamespaced = JSON.parse(response.getReturnValue());
+                if (isOrgNamespaced) {
+                    var listViewNamespaced = {
+                        'objectApiName': 'npsp__DataImportBatch__c',
+                        'listName': 'npsp__Gift_Batches',
+                        'isLoaded': true
+                    };
+                    component.set('v.listView', listViewNamespaced);
+                } else {
+                    var listViewUnnamespaced = {
+                        'objectApiName': 'DataImportBatch__c',
+                        'listName': 'Gift_Batches',
+                        'isLoaded': true
+                    };
+                    component.set('v.listView', listViewUnnamespaced);
+                }
+            } else {
+                this.handleApexErrors(component, response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    /**
+     * @description: handles the display of errors from an apex callout
+     * @param errors: list of potential errors passed back from apex
+     */
+    handleApexErrors: function(component, errors) {
+        debugger;
+        let message;
+        if (errors && errors[0] && errors[0].message) {
+            message = errors[0].message;
+        } else {
+            message = 'Unknown error';
+        }
+        this.showToast(component, $A.get('$Label.c.PageMessagesError'), message, 'error');
+    },
+
+    /**
+     * @description: checks that user has all necessary permissions and then launches modal or displays error
+     */
+    openNewBatchWizard: function(component) {
+        debugger;
+        var action = component.get('c.checkFieldPermissions');
+        action.setParams({sObjectName: component.get('v.listView.objectApiName')});
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var modalBody;
+                var modalHeader;
+                var modalFooter;
+
+                $A.createComponents([
+                        ['c:BGE_ConfigurationWizard', {sObjectName: 'DataImportBatch__c'}],
+                        ['c:modalHeader', {header: $A.get('$Label.c.bgeBatchInfoWizard')}],
+                        ['c:modalFooter', {}]
+                    ],
+                    function(components, status, errorMessage){
+                        if (status === 'SUCCESS') {
+                            modalBody = components[0];
+                            modalHeader = components[1];
+                            modalFooter = components[2];
+                            component.find('overlayLib').showCustomModal({
+                                body: modalBody,
+                                header: modalHeader,
+                                footer: modalFooter,
+                                showCloseButton: true,
+                                cssClass: 'slds-modal_large'
+                            })
+                        } else {
+                            this.showToast(component, $A.get('$Label.c.PageMessagesError'), errorMessage, 'error');
+                        }
+                    }
+                );
+            } else if (state === 'ERROR') {
+                console.log(response.getError());
+                this.handleApexErrors(component, response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    /**
+     * @description: displays standard toast to user based on success or failure of their action
+     * @param title: Title displayed in toast
+     * @param message: body of message to display
+     * @param type: configures type of toast
+     */
+    showToast: function(component, title, message, type) {
+        var mode = (type === 'error') ? 'sticky' : 'pester';
+
+        component.find('notifLib').showToast({
+            'variant': type,
+            'mode': mode,
+            'title': title,
+            'message': message
+        });
+    },
+
+})
