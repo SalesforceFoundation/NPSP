20fa6b93944c5e1271012b5b38902f86eb89fbcf
Ken Lewis <kenneth.lewis@salesforce.com>
2019-07-02 08:11:26 -0700

    Add platform event listener component into Advanced Mapping container component

--- src/aura/BDI_ManageAdvancedMapping/BDI_ManageAdvancedMappingController.js
+++ src/aura/BDI_ManageAdvancedMapping/BDI_ManageAdvancedMappingController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
+    handleDeploymentNotification: function (component, event) {
+        console.log('in top-level AURA component, handleDeploymentNotification');
+        let deploymentId = event.getParam('deploymentId');
+        console.log('event.getParam(deploymentId): ', deploymentId);
+        component.find("platformEventListener").registerDeploymentId(deploymentId);
     }
 })
