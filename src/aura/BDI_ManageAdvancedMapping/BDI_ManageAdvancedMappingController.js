f0569d2cb59d7e2bffd613fb8ed19778f4564fb3
Ken Lewis <kenneth.lewis@salesforce.com>
2019-07-02 10:22:29 -0700

    Add deployment event handler to top-level Aura component

--- src/aura/BDI_ManageAdvancedMapping/BDI_ManageAdvancedMappingController.js
+++ src/aura/BDI_ManageAdvancedMapping/BDI_ManageAdvancedMappingController.js
@@ -1,8 +1,6 @@
 ({
     handleDeploymentNotification: function (component, event) {
-        console.log('in top-level AURA component, handleDeploymentNotification');
         let deploymentId = event.getParam('deploymentId');
-        console.log('event.getParam(deploymentId): ', deploymentId);
         component.find("platformEventListener").registerDeploymentId(deploymentId);
     }
 })
