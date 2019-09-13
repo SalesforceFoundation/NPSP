211dbc7e2988a11d883aace287dd65faa8e75256
Paul Prescod <pprescod@salesforce.com>
2019-09-12 21:27:32 -0700

    Delete some cruft.

--- build_org.sh
+++
@@ -1,6 +0,0 @@
-export org=dev_namespaced_test2
-echo $org
-cci org scratch_delete $org
-cci org scratch --days 7 --default dev_namespaced $org
-time cci flow run qa_org_namespaced --org $org
-cci task run turn_on_debug_logs --org $org
