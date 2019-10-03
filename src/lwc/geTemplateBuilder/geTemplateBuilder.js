f13ee19694577da672431c106605cd6b4937fef1
Prince Sadie <psadie@salesforce.com>
2019-10-02 23:21:51 -0700

    add comments to geTemplateBuilder

--- src/lwc/geTemplateBuilder/geTemplateBuilder.js
+++ src/lwc/geTemplateBuilder/geTemplateBuilder.js
@@ -14,23 +14,6 @@ export default class geTemplateBuilder extends NavigationMixin(LightningElement)
         new FormLayout()
     );
 
-    /* delete once all sections are their own components, currently only used by
-    * matching logic tab */
-    handleToggleClick(event) {
-        this._previousTab = this.activeTab;
-        this.activeTab = event.target.getAttribute('data-tab-value');
-    }
-
-    /*******************************************************************************
-    * @description Lifecycle hook that gets called after every render of the component.
-    * Need to reset the activeTab for lightning:tabset to rerender properly because
-    * clicking on the individual lightning:tab components does not auto-update the
-    * lightning:tabset active-tab-value property.
-    */
-    renderedCallback() {
-        this.activeTab = undefined;
-    }
-
     /* Needs to be revisited, WIP tied to retrieving and rendering an existing template */
     get templateInfo() {
         return { name: this.formTemplate.name, description: this.formTemplate.description };
@@ -51,39 +34,54 @@ export default class geTemplateBuilder extends NavigationMixin(LightningElement)
         return this.formTemplate.layout.fieldMappingSetDevName ? this.formTemplate.layout.fieldMappingSetDevName : undefined;
     }
 
-    handleFormTemplateSave() {
-        console.log('handleFormTemplateSave');
+    /*******************************************************************************
+    * @description Lifecycle hook that gets called after every render of the component.
+    * Need to reset the activeTab for lightning:tabset to rerender properly because
+    * clicking on the individual lightning:tab components does not auto-update the
+    * lightning:tabset active-tab-value property.
+    */
+    renderedCallback() {
+        this.activeTab = undefined;
+    }
+
+    /*******************************************************************************
+    * @description Handles previous and next tab navigation
+    *
+    * @param {object} event: Event received from child component
+    */
+    handleGoToTab(event) {
+        this.activeTab = event.detail.tabValue;
+    }
+
+    /*******************************************************************************
+    * @description Function queries for the builder's various child components,
+    * calls a data getter method on each child component, and populates properties
+    * on the FormTemplate object. Pass the FormTemplate JSON to apex and awaits
+    * for a record id so we can navigate to the inserted Form_Template__c detail page
+    */
+    handleFormTemplateSave = async () => {
         const templateInfo = this.template.querySelector('c-ge-template-builder-template-info').getTabData();
-        console.log('Template Info Tab: ', mutable(templateInfo));
         const batchHeader = this.template.querySelector('c-ge-template-builder-batch-header').getTabData();
-        console.log('Batch Header Tab: ', mutable(batchHeader));
         const formLayout = this.template.querySelector('c-ge-template-builder-gift-fields').getTabData();
-        console.log('Gift Fields Tab: ', mutable(formLayout));
 
         this.formTemplate.name = templateInfo.name;
         this.formTemplate.description = templateInfo.description;
         this.formTemplate.batchHeaderFields = batchHeader;
         this.formTemplate.layout = formLayout;
 
-        console.log('*************');
-        console.log('FORM TEMPLATE');
-        console.log('*************');
+        const preppedFormTemplate = {
+            templateJSON: JSON.stringify(this.formTemplate),
+            templateName: this.formTemplate.name
+        };
+        const formTemplateRecordId = await processFormTemplate(preppedFormTemplate);
 
-        console.log(mutable(this.formTemplate));
-
-        processFormTemplate({templateJSON: JSON.stringify(this.formTemplate), templateName: this.formTemplate.name})
-            .then(result => {
-                console.log('Result: ', result);
-                this.navigateToRecordViewPage(result);
-            })
-            .catch(error => {
-                console.log('Error: ', error);
-            })
-        console.log('Attempted to send to apex');
+        this.navigateToRecordViewPage(formTemplateRecordId);
     }
 
+    /*******************************************************************************
+    * @description Navigates to a record detail page by record id
+    */
     navigateToRecordViewPage(recordId) {
-        // View a custom object record.
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
             attributes: {
@@ -105,34 +103,10 @@ export default class geTemplateBuilder extends NavigationMixin(LightningElement)
             })
     }
 
-    /*******************************************************************************
-    * @description Handles previous and next tab navigation
-    *
-    * @param {object} event: Event received from child component
-    */
-    handleGoToTab(event) {
-        this.activeTab = event.detail.tabValue;
-
-        /*if (event.detail.tabData) {
-            this.stashTabDetails(event.detail.tabData);
-        }*/
+    /* delete once all sections are their own components, currently only used by
+    * matching logic tab */
+    handleToggleClick(event) {
+        this._previousTab = this.activeTab;
+        this.activeTab = event.target.getAttribute('data-tab-value');
     }
-
-    /* Placeholder function to stash tab details (template info, selected batch header fields, etc)
-    * into an object.
-    */
-    /*stashTabDetails(tabData) {
-        //console.log(this.mutable(tabData));
-        if (tabData.sourceTab === 'geTemplateBuilderTemplateInfo') {
-            console.log('Stashing Template Info | geTemplateBuilderTemplateInfo');
-            console.log('Form Template:::', this.formTemplate);
-            this.formTemplate.name = tabData.name;
-            this.formTemplate.description = tabData.description;
-        } else if (tabData.sourceTab === 'geTemplateBuilderBatchHeader') {
-            console.log('Stashing Template Info | hbeTemplateBuilderBatchHeader');
-            this.formTemplate.batchHeader = tabData.batchFields;
-        }
-
-        console.log('Form Template: ', JSON.parse(JSON.stringify(this.formTemplate)));
-    }*/
 }
