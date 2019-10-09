0eb9df6d5e7731a8f7106583dbc4fb9fd6ee6b48
David Reed <d.reed@salesforce.com>
2019-10-09 15:40:55 -0600

    Use namespace prefix

--- tasks/check_rd2_enablement.py
+++ tasks/check_rd2_enablement.py
@@ -5,7 +5,7 @@ class is_rd2_enabled(BaseSalesforceApiTask):
     def _run_task(self):
         try:
             settings = self.sf.query(
-                "SELECT IsRecurringDonations2Enabled__c "
+                "SELECT npsp__IsRecurringDonations2Enabled__c "
                 "FROM npe03__Recurring_Donations_Settings__c "
                 "WHERE SetupOwnerId IN (SELECT Id FROM Organization)"
             )
