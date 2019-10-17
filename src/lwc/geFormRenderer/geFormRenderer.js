de6fd0ad4624590c9760d23c708982a49418034d
Vladimir Martinov <vmartinov@vmartinov-ltm.internal.salesforce.com>
2019-10-17 10:46:49 -0700

    get the template and sections from the json

--- src/lwc/geFormRenderer/geFormRenderer.js
+++ src/lwc/geFormRenderer/geFormRenderer.js
@@ -2,5 +2,33 @@ import {LightningElement, track} from 'lwc';
 import { getFormTemplate } from 'c/geFormService';
 
 export default class GeFormRenderer extends LightningElement {
+    @track sections = [];
+    @track ready = false;
+    @track name = '';
+    @track description = '';
+    @track mappingSet = '';
+    @track version = '';
+
+    connectedCallback() {
+        getFormTemplate().then(response => {
+
+            // read the template header info
+            if(response !== null && typeof response !== 'undefined') {
+                this.ready = true;
+                this.name = response.name;
+                this.description = response.description;
+                this.mappingSet = response.layout.fieldMappingSetDevName;
+                this.version = response.layout.version;
+
+                if (response.layout !== null && typeof response.layout !== 'undefined' && Array.isArray(response.layout.sections)) {
+                    this.sections = response.layout.sections;
+                    
+                }
+            }
+
+        });
+    }
+
+
 
 }
