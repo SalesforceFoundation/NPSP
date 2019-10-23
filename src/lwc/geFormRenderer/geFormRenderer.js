94941ab07f775a26b72f7fd7e7478999a32a4376
Benjamin Dvorachek <bdvorachek@salesforce.com>
2019-10-23 11:09:02 -0500

    Post merge cleanup for retreiving layout

--- src/lwc/geFormRenderer/geFormRenderer.js
+++ src/lwc/geFormRenderer/geFormRenderer.js
@@ -9,30 +9,26 @@ export default class GeFormRenderer extends LightningElement {
     @track mappingSet = '';
     @track version = '';
 
-    // connectedCallback() {
-    //     getFormTemplate().then(response => {
-
-    //         // read the template header info
-    //         if(response !== null && typeof response !== 'undefined') {
-    //             this.ready = true;
-    //             this.name = response.name;
-    //             this.description = response.description;
-    //             this.mappingSet = response.layout.fieldMappingSetDevName;
-    //             this.version = response.layout.version;
-
-    //             if (response.layout !== null && typeof response.layout !== 'undefined' && Array.isArray(response.layout.sections)) {
-    //                 this.sections = response.layout.sections;              
-    //             }
-    //         }
-    //     });
-    // }
-
     connectedCallback() {
-        getFormTemplate()
-        .then(response => {
-            this.sections = response.formTemplate.layout.sections;
+        getFormTemplate().then(response => {
+
+            // read the template header info
+            if(response !== null && typeof response !== 'undefined') {
+                this.ready = true;
+                this.name = response.name;
+                this.description = response.description;
+                this.mappingSet = response.layout.fieldMappingSetDevName;
+                this.version = response.layout.version;
+
+                if (response.layout !== null && typeof response.layout !== 'undefined' && Array.isArray(response.layout.sections)) {
+                    this.sections = response.layout.sections;
+
+                }
+            }
+
         });
     }
+
 
 
 }
