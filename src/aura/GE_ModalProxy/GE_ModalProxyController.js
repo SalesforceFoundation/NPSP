277c8cea539b5a4b0f887b31bc79d246f343563a
Prince Sadie <psadie@salesforce.com>
2019-10-23 10:46:26 -0700

    rename modal proxy aura component

--- src/aura/GE_ModalProxy/GE_ModalProxyController.js
+++ src/aura/GE_ModalProxy/GE_ModalProxyController.js
@@ -1,13 +1,10 @@
 ({
     handleSave : function(component, event, helper) {
-        console.log('Handle Save Here');
-        console.log(JSON.parse(JSON.stringify(event.getParams('section'))));
         component.set('v.sectionBeingEdited', event.getParams('section'));
         component.find("overlayLib").notifyClose();
     },
 
     handleCancel : function(component, event, helper) {
-        console.log('Handle Cancel Here');
         component.find("overlayLib").notifyClose();
     }
 })
