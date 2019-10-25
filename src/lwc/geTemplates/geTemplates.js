69b355e2b9f7becb8e9381a497633dfc5960ffd0
Prince Sadie <psadie@salesforce.com>
2019-10-24 19:49:15 -0700

    add custom lightning tabs for template builder and placeholder landing page, add geTemplates component, and update package.xml

---
+++ src/lwc/geTemplates/geTemplates.js
@@ -0,0 +1,79 @@
+import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getAllFormTemplates from '@salesforce/apex/GE_TemplateBuilderCtrl.getAllFormTemplates';
+import deleteFormTemplate from '@salesforce/apex/GE_TemplateBuilderCtrl.deleteFormTemplate';
+import { findIndexByProperty } from 'c/utilTemplateBuilder';
+
+const actions = [
+    { label: 'Edit', name: 'edit' },
+    { label: 'Delete', name: 'delete' }
+];
+
+const columns = [
+    { label: 'Template Name', fieldName: 'Name' },
+    { label: 'Template Description', fieldName: 'npsp__Description__c' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    },
+];
+
+export default class GeTemplates extends NavigationMixin(LightningElement) {
+    @track templates;
+    @track columns = columns;
+    @track isLoading = true;
+
+    connectedCallback() {
+        this.init();
+    }
+
+    init = async () => {
+        this.templates = await getAllFormTemplates();
+        this.isLoading = false;
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+
+        switch (actionName) {
+            case 'edit':
+                this.navigateToTemplateBuilder(row.Id);
+                break;
+            case 'delete':
+                deleteFormTemplate({ id: row.Id }).then(() => {
+                    const index = findIndexByProperty(this.templates, 'Id', row.Id);
+                    this.templates.splice(index, 1);
+                    this.templates = [...this.templates];
+                });
+                break;
+            default:
+        }
+    }
+
+    /*******************************************************************************
+    * @description Navigates to the Template Builder. If a recordId is provided,
+    * adds the recordId to the navigation state (query param).
+    *
+    * @param {string} recordId: Record id of the Form_Template__c
+    */
+    navigateToTemplateBuilder(recordId) {
+        const queryParameter = recordId ? { c__recordId: recordId } : {};
+
+        this[NavigationMixin.Navigate]({
+            type: 'standard__navItemPage',
+            attributes: {
+                apiName: 'npsp__GE_Template_Builder'
+            },
+            state: queryParameter
+        });
+    }
+
+    /*******************************************************************************
+    * @description Handles onclick event from 'Create Template' button and navigates
+    * to the Template Builder.
+    */
+    handleNewFormTemplate() {
+        this.navigateToTemplateBuilder();
+    }
+}
