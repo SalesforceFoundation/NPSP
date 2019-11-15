ab9d76b262a1908830177c2bc40062a3540995e5
Ken Lewis <kenneth.lewis@salesforce.com>
2019-11-15 10:32:47 -0800

    Gift Entry - Batch Mode
    Add support for the gift entry form to operate in batch mode, which
    saves data import records, relates them to a batch, and runs the
    dry run process.  As the form is submitted a new row is placed in the
    table and dry run results update the row in the table when completed.

--- src/lwc/geFormRenderer/geFormRenderer.js
+++ src/lwc/geFormRenderer/geFormRenderer.js
@@ -6,13 +6,15 @@ import geSave from '@salesforce/label/c.labelGeSave';
 import geCancel from '@salesforce/label/c.labelGeCancel';
 
 export default class GeFormRenderer extends NavigationMixin(LightningElement) {
-    @track sections = [];
+    @api sections = [];
     @track ready = false;
     @track name = '';
     @track description = '';
     @track mappingSet = '';
     @track version = '';
     @api showSpinner = false;
+    @api isBatchMode = false;
+    @api submissions = [];
     label = { messageLoading, geSave, geCancel };
 
     connectedCallback() {
@@ -28,10 +30,24 @@ export default class GeFormRenderer extends NavigationMixin(LightningElement) {
                 if (typeof formTemplate.layout !== 'undefined'
                         && Array.isArray(formTemplate.layout.sections)) {
                     this.sections = formTemplate.layout.sections;
+                    if (this.isBatchMode) {
+                        this.dispatchEvent(new CustomEvent('sectionsretrieved'));
+                    }
                 }
             }
 
         });
+    }
+
+    @api
+    setBatchMode(batchId) {
+        if (batchId) {
+            this.isBatchMode = true;
+            this.dispatchEvent(new CustomEvent('sectionsretrieved')); //Sets the columns
+            // on the table
+        } else {
+            this.isBatchMode = false;
+        }
     }
 
     handleCancel() {
@@ -47,10 +63,20 @@ export default class GeFormRenderer extends NavigationMixin(LightningElement) {
         // TODO: Pass the actual Data Import record, and navigate to the new Opportunity
         // const OpportunityId = GeFormService.createOpportunityFromDataImport(dataImport);
         const sectionsList = this.template.querySelectorAll('c-ge-form-section');
-        
-        GeFormService.handleSave(sectionsList).then(opportunityId => {
-            this.navigateToRecordPage(opportunityId);
-        });
+
+        if (this.isBatchMode) {
+            const submission = {
+                sectionsList: sectionsList,
+                submissionId: this.submissions.length
+            };
+            this.submissions.push(submission);
+            this.dispatchEvent(new CustomEvent('submit'));
+            this.toggleSpinner();
+        } else {
+            GeFormService.handleSave(sectionsList).then(opportunityId => {
+                this.navigateToRecordPage(opportunityId);
+            });
+        }
     }
 
     navigateToRecordPage(recordId) {
