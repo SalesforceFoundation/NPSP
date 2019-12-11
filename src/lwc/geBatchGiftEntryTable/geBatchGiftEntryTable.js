ef232972c4880581a4128298c9420d371838fe27
Ken Lewis <kenneth.lewis@salesforce.com>
2019-12-11 12:20:39 -0800

    HGE: Display table only after columns are ready and batch is loaded

--- src/lwc/geBatchGiftEntryTable/geBatchGiftEntryTable.js
+++ src/lwc/geBatchGiftEntryTable/geBatchGiftEntryTable.js
@@ -87,6 +87,18 @@ export default class GeBatchGiftEntryTable extends LightningElement {
         this.setReady();
     }
 
+    //todo
+    // getData(dataImportRows){
+    //     return dataImportRows.map(row => {
+    //         const record = row.record;
+    //         record.donorLink = row.donorLink;
+    //         record.donorName = row.donorName;
+    //         record.matchedRecordLabel = row.matchedRecordLabel;
+    //         record.matchedRecordUrl = row.matchedRecordUrl;
+    //         record.errors = row.errors.join(', ');
+    //     });
+    // }
+
     @api
     handleSectionsRetrieved(sections) {
         this.initColumns(this.buildColumns(sections));
