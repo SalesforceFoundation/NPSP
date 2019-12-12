daf1466ddaaf80397b9e2115bcf636251273e548
Julian Joseph <Julian88Tex@gmail.com>
2019-12-12 00:19:23 -0800

    create qa rebuild script

---
+++ scripts/qa_pull_and_rebuild.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Script to be used by qa to rebuild a scratch environment.
+# To run paste into command line "bash scripts/qa_rebuild.sh <orgname> <branch>
+# For example "bash scripts/qa_rebuild.sh W-037818 feature/RD2__rad-data-migration-display-errors"
+
+# ***IMPORTANT*** This script will rollback any modifications to files and delete a scratch org with the same name.
+
+# Remove any existing modified files so a pull can take place
+git checkout -- .
+
+# Switch to desired branch
+git checkout $2
+
+# Pull most recent changes
+git pull
+
+# Delete exisiting scratch org if one exists with provided name
+cci org scratch_delete $1
+
+# Rebuild org with qa flow
+cci flow run qa_org --org $1
+
+# Mark org as default
+cci org default $1
+
+# Open newly created org
+cci org browser $1
