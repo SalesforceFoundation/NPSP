d0c066954ca4c4453801e83e4749087bfe20c2ca
Julian Joseph <Julian88Tex@gmail.com>
2019-12-12 11:12:45 -0800

    corrected warning to mention staging changes

--- scripts/qa_pull_and_rebuild.sh
+++ scripts/qa_pull_and_rebuild.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
 # Script to be used by qa to rebuild a scratch environment.
-
+echo 
+echo ***WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING***
+echo 
 echo This script is to be used by qa to rebuild a scratch environment.
-echo ***WARNING*** This script will rollback any modifications to files and delete a scratch org with the same name given. Please commit any file changes before running this script. ***WARNING***
+echo This script will rollback any modifications to files and delete a scratch org with the same name given.
+echo Please save and stage any file changes before running this script or they will be undone.
+echo 
+echo ***WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING******WARNING***
+echo 
 
+# Prompt for conifrmation
 read -p "Are you sure? [Y/n]" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-echo What branch would you like to pull most recent changes from?? [example: feature/julian-qa-rebuild-script]
-read branch_name
-
-echo What would you like to name your org? [example: W-123456]
-read org_name
-
+# Request branch name
+read -p "What branch would you like to pull most recent changes from?? [example: feature/julian-qa-rebuild-script] " -r branch_name
+ 
+# Request org name
+read -p "What would you like to name your org? [example: W-123456]" org_name
 
 # Remove any existing modified files so a pull can take place
 git checkout -- .
