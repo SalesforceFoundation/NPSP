a0add134bfbc256796b938c0caf5726951523930
Kiet Truong <kiet.truong@salesforce.com>
2020-01-14 15:59:54 -0800

    Swapped an @api back to @track

--- src/lwc/geFormRenderer/geFormRenderer.js
+++ src/lwc/geFormRenderer/geFormRenderer.js
@@ -13,7 +13,7 @@ export default class GeFormRenderer extends NavigationMixin(LightningElement) {
     @api recordId = '';
     @api record;
     fieldNames = [];
-    @api sections = [];
+    @track sections = [];
     @track ready = false;
     @track name = '';
     @track description = '';
