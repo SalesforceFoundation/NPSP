5eca3c3ebb61af0f1356f53a54b5df345712b192
Prince Sadie <psadie@salesforce.com>
2020-01-14 21:38:39 -0800

    update template builder clone functionality

--- src/lwc/geTemplates/geTemplates.js
+++ src/lwc/geTemplates/geTemplates.js
@@ -5,6 +5,7 @@ import cloneFormTemplate from '@salesforce/apex/FORM_ServiceGiftEntry.cloneFormT
 import getDataImportSettings from '@salesforce/apex/UTIL_CustomSettingsFacade.getDataImportSettings';
 import TemplateBuilderService from 'c/geTemplateBuilderService';
 import { dispatch, handleError, showToast } from 'c/utilTemplateBuilder';
+import { isEmpty } from 'c/utilCommon';
 import GeLabelService from 'c/geLabelService';
 
 import FORM_TEMPLATE_INFO from '@salesforce/schema/Form_Template__c';
@@ -203,16 +204,7 @@ export default class GeTemplates extends NavigationMixin(LightningElement) {
                 this.navigateToTemplateBuilder(row.Id);
                 break;
             case 'clone':
-                this.geListViewComponent.setProperty(IS_LOADING, true);
-
-                cloneFormTemplate({ id: row.Id })
-                    .then((clonedTemplate) => {
-                        this.geListViewComponent.setProperty(IS_LOADING, false);
-                        this.geListViewComponent.refresh();
-                    })
-                    .catch(error => {
-                        handleError(error);
-                    });
+                this.navigateToTemplateBuilder(row.Id, { c__clone: true });
                 break;
             case 'delete':
                 this.geListViewComponent.setProperty(IS_LOADING, true);
@@ -276,8 +268,11 @@ export default class GeTemplates extends NavigationMixin(LightningElement) {
     *
     * @param {string} recordId: Record id of the Form_Template__c
     */
-    navigateToTemplateBuilder(recordId) {
-        const queryParameter = recordId ? { c__recordId: recordId } : {};
+    navigateToTemplateBuilder(recordId, additionalQueryParameters) {
+        let queryParameter = recordId ? { c__recordId: recordId } : {};
+        if (!isEmpty(additionalQueryParameters)) {
+            queryParameter = { ...queryParameter, ...additionalQueryParameters };
+        }
 
         this[NavigationMixin.Navigate]({
             type: 'standard__navItemPage',
