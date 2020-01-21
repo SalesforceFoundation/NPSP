ac1cccf42f5c9697a0841d340b3cce0d27a574d4
Gustavo Rivero <grivero@salesforce.com>
2020-01-20 16:45:10 -0800

    new label for donation donor validation

--- src/lwc/geLabelService/geLabelService.js
+++ src/lwc/geLabelService/geLabelService.js
@@ -67,6 +67,7 @@ import geButtonTemplatesTabCreateTemplate from '@salesforce/label/c.geButtonTemp
 import geErrorCompleteThisField from '@salesforce/label/c.geErrorCompleteThisField';
 import geErrorExistingTemplateName from '@salesforce/label/c.geErrorExistingTemplateName';
 import geErrorDonorTypeValidation from '@salesforce/label/c.geErrorDonorTypeValidation';
+import geErrorDonorTypeValidationSingle from '@salesforce/label/c.geErrorDonorTypeValidationSingle';
 import geErrorFieldPermission from '@salesforce/label/c.geErrorFieldPermission';
 import geErrorPageLevelAdvancedMappingBody from '@salesforce/label/c.geErrorPageLevelAdvancedMappingBody';
 import geErrorPageLevelAdvancedMappingHeader from '@salesforce/label/c.geErrorPageLevelAdvancedMappingHeader';
@@ -193,6 +194,7 @@ class GeLabelService {
         geButtonTemplatesTabCreateTemplate,
         geErrorCompleteThisField,
         geErrorDonorTypeValidation,
+        geErrorDonorTypeValidationSingle,
         geErrorExistingTemplateName,
         geErrorFieldPermission,
         geErrorPageLevelAdvancedMappingBody,
