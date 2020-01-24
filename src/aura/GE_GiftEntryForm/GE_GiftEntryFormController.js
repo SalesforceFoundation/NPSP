376a015b47caef3da1b25999a50a9913599fcc2f
Ken Lewis <kenneth.lewis@salesforce.com>
2020-01-24 10:59:43 -0800

    Enable close button functionality on edit batch info modal

--- src/aura/GE_GiftEntryForm/GE_GiftEntryFormController.js
+++ src/aura/GE_GiftEntryForm/GE_GiftEntryFormController.js
@@ -1,22 +1,34 @@
 ({
     handleEditBatch: function (component) {
 
-        $A.createComponents([["c:geBatchWizard", {recordId: component.get('v.recordId')}]],
+        const closeModal = function (component) {
+            component.find('giftEntryFormOverlayLib').notifyClose();
+        };
+
+        $A.createComponents([["c:geBatchWizard", {
+                recordId: component.get('v.recordId'),
+                dedicatedListenerEventName: 'geBatchWizardEvent',
+            }]],
             function (components, status, errorMessage) {
                 if (status === "SUCCESS") {
-                    const modalBody = components[0];
-
-                    let modalReference = component.find('overlayLib').showCustomModal({
-                        body: modalBody,
-                        cssClass: "slds-modal_large",
-                    });
-
+                    let modalReference =
+                        component.find('giftEntryFormOverlayLib').showCustomModal({
+                            body: components[0],
+                            cssClass: "slds-modal_large",
+                            closeModal: closeModal,
+                        });
                     component.set('v.modal', modalReference);
                 } else {
                     console.error(errorMessage);
                 }
             }
         );
-    }
+    },
+
+    handleBatchWizardEvent: function (component) {
+        component.get('v.modal').then(modal => {
+            modal.close();
+        });
+    },
 
 });
