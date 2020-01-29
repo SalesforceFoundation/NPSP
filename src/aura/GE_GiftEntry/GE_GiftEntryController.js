6661b128a2614efef72cb9e9d56f03322e7c2d9a
Kiet Truong <kiet.truong@salesforce.com>
2020-01-28 21:21:14 -0800

    added default template creation on BGE aura component

--- src/aura/GE_GiftEntry/GE_GiftEntryController.js
+++ src/aura/GE_GiftEntry/GE_GiftEntryController.js
@@ -14,7 +14,16 @@
                 // do something
             }
             else if (state === "ERROR") {
+                var errors = response.getError();
                 
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
             }
         });
 
--- src/aura/GE_GiftEntryForm/GE_GiftEntryFormController.js
+++ src/aura/GE_GiftEntryForm/GE_GiftEntryFormController.js
@@ -1,4 +1,33 @@
 ({
+    doInit: function(cmp) {
+        var action = cmp.get("c.createDefaultTemplate");
+
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+
+            if (state === "SUCCESS") {
+
+                cmp.set('v.finishedInit', true);
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+
+        $A.enqueueAction(action);
+    },   
     handleEditBatch: function (component) {
 
         const closeModal = function (component) {
