b87a0a1b4b11cc29b233c0e255a6b8ed10e6e3f6
Andrew Yu <andrew.yu@salesforce.com>
2020-04-14 17:55:04 -0700

    gather all required functions from EnableDelegate cmp

--- src/aura/RD2_EnablementDelegateSimple/RD2_EnablementDelegateSimpleController.js
+++ src/aura/RD2_EnablementDelegateSimple/RD2_EnablementDelegateSimpleController.js
@@ -3,7 +3,18 @@
         helper.loadState(component);
         helper.getDeployURL(component);
     },
-
+    handleEnableConfirm: function (component, event, helper) {
+        helper.confirmEnable(component);
+    },
+    handleEnable: function (component, event, helper) {
+        helper.completeEnable(component);
+    },
+    handleMetaDeployLaunch: function (component, event, helper) {
+        helper.launchDeploy(component);
+    },
+    handleMetaDeployConfirm: function (component, event, helper) {
+        helper.confirmDeploy(component);
+    },
     refreshView: function (component, event, helper) {
         helper.refreshView(component);
     }
--- src/aura/RD2_EnablementDelegateSimple/RD2_EnablementDelegateSimpleHelper.js
+++ src/aura/RD2_EnablementDelegateSimple/RD2_EnablementDelegateSimpleHelper.js
@@ -1,10 +1,207 @@
 ({
     /****
+    * @description Clears the errors on the page
+    */
+   clearError: function (component) {
+        component.set('v.errorSection', '');
+        component.set('v.errorMessage', '');
+    },
+
+    /****
+    * @description Confirms MetaDeploy has been deployed
+    */
+   confirmDeploy: function (component) {
+    //disable the current active step so the next step is enabled only on current step success
+    component.set('v.state.isMetaConfirmed', false);
+    this.showSpinner(component, 'metadeployConfirmSpinner');
+
+    component.set('v.state.isDryRunInProgress', false);
+    component.set('v.state.isDryRunStopped', false);
+
+    this.clearError(component);
+
+    var action = component.get('c.confirmMetaDeploy');
+
+    action.setCallback(this, function (response) {
+        if (!component.isValid()) {
+            return;
+        }
+        const state = response.getState();
+
+        if (state === 'SUCCESS') {
+            component.set('v.state.isMetaConfirmed', true);
+
+        } else if (state === 'ERROR') {
+            this.handleError(component, response.getError(), 'metadeploy');
+        }
+
+        this.refreshMetaDeploy(component);
+        this.hideSpinner(component, 'metadeployConfirmSpinner');
+    });
+
+    $A.enqueueAction(action);
+    },
+
+    /****
+    * @description Confirms enhanced Recurring Donations enablement
+    */
+   confirmEnable: function (component) {
+        //disable the current active step so the next step is enabled only on current step success
+        component.set('v.state.isConfirmed', false);
+        this.showSpinner(component, 'enableConfirmSpinner');
+        this.clearError(component);
+
+        var action = component.get('c.confirmEnablement');
+
+        action.setCallback(this, function (response) {
+            if (!component.isValid()) {
+                return;
+            }
+            const state = response.getState();
+
+            if (state === 'SUCCESS') {
+                component.set('v.state.hideDryRun', true);
+                component.set('v.state.isConfirmed', true);
+
+            } else if (state === 'ERROR') {
+                this.handleError(component, response.getError(), 'enablement');
+            }
+
+            this.refreshEnable(component);
+            this.hideSpinner(component, 'enableConfirmSpinner');
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    /****
+    * @description Enables enhanced Recurring Donations
+    */
+   completeEnable: function (component) {
+        //disable the current active step so the next step is enabled only on current step success
+        component.set('v.state.isEnabled', false);
+        this.clearError(component);
+        this.disableEdit(component, "enableToggle");
+
+        var action = component.get('c.enableEnhancement');
+
+        action.setCallback(this, function (response) {
+            if (!component.isValid()) {
+                return;
+            }
+            const state = response.getState();
+
+            if (state === 'SUCCESS') {
+                component.set('v.state.isEnabled', true);
+
+            } else if (state === 'ERROR') {
+                this.handleError(component, response.getError(), 'enablement');
+            }
+
+            this.refreshEnable(component);
+            this.refreshMetaDeploy(component);
+
+            // notify NPSP Settings page about enhanced Recurring Donation enablement
+            var event = $A.get("e.c:RD2_EnhancementEnabledEvent");
+            event.fire();
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    /****
+    * @description Disables input field edit
+    */
+    disableEdit: function (component, inputName) {
+        let inputComp = component.find(inputName);
+        $A.util.addClass(inputComp, "disabledEdit");
+    },
+
+    /****
     * @description Displays component's element
     */
-   displayElement: function (component, elementName) {
-    let element = component.find(elementName);
-    $A.util.removeClass(element, "slds-hide");
+    displayElement: function (component, elementName) {
+        let element = component.find(elementName);
+        $A.util.removeClass(element, "slds-hide");
+    },
+
+    /****
+    * @description Enables input field edit
+    */
+   enableEdit: function (component, inputName) {
+    let inputComp = component.find(inputName);
+    $A.util.removeClass(inputComp, "disabledEdit");
+    },
+
+    /**
+     * @description: Displays errors thrown by Apex method invocations
+     * @param errors: Error list
+     */
+    handleError: function (component, errors, section) {
+        let message;
+        if (errors && errors[0] && errors[0].message) {
+            message = errors[0].message;
+
+        } else if (errors && errors.message) {
+            message = errors.message;
+
+        } else {
+            message = $A.get('$Label.c.stgUnknownError');
+        }
+
+        component.set('v.errorSection', section);
+        component.set('v.errorMessage', message);
+    },
+
+    /****
+    * @description Loads the enablement state and enables/disables page elements based on it
+    */
+   getDeployURL: function (component) {
+    var action = component.get("c.getMetaDeployURL");
+
+    action.setCallback(this, function (response) {
+        if (!component.isValid()) {
+            return;
+        }
+        let state = response.getState();
+
+        if (state === "SUCCESS") {
+            const metaDeployURL = response.getReturnValue();
+            component.set('v.metaDeployURL', metaDeployURL);
+
+        } else if (state === "ERROR") {
+            component.set('v.metaDeployURL', 'https://install.salesforce.org/products/npsp/enhanced-recurring-donations');
+        }
+    });
+
+    $A.enqueueAction(action);
+    },
+
+    /****
+    * @description Confirms MetaDeploy has been launched
+    */
+   launchDeploy: function (component) {
+        this.clearError(component);
+
+        var action = component.get('c.launchMetaDeploy');
+
+        action.setCallback(this, function (response) {
+            if (!component.isValid()) {
+                return;
+            }
+            const state = response.getState();
+
+            if (state === 'SUCCESS') {
+                component.set('v.state.isMetaLaunched', true);
+
+            } else if (state === 'ERROR') {
+                this.handleError(component, response.getError(), 'metadeploy');
+            }
+
+            this.refreshMetaDeploy(component);
+        });
+
+        $A.enqueueAction(action);
     },
 
     /****
@@ -18,7 +215,7 @@
                 return;
             }
             const state = response.getState();
-
+            console.log(response.getReturnValue());
             if (state === "SUCCESS") {
                 const enablementState = JSON.parse(response.getReturnValue());
 
@@ -100,5 +297,21 @@
     hideElement: function (component, elementName) {
         let element = component.find(elementName);
         $A.util.addClass(element, "slds-hide");
+    },
+
+    /**
+     * @description: hides specific spinner
+     */
+    hideSpinner: function (component, element) {
+        var spinner = component.find(element);
+        $A.util.addClass(spinner, 'slds-hide');
+    },
+
+    /**
+     * @description: shows specific spinner
+     */
+    showSpinner: function (component, element) {
+        var spinner = component.find(element);
+        $A.util.removeClass(spinner, 'slds-hide');
     }
 })
