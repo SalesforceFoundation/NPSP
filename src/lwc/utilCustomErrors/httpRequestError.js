01759296531fa5f132ae56621778c318cc602552
Prince Sadie <psadie@salesforce.com>
2020-04-24 22:56:55 -0700

    add jsdoc to custom errors and update package xml

--- src/lwc/utilCustomErrors/httpRequestError.js
+++ src/lwc/utilCustomErrors/httpRequestError.js
@@ -1,4 +1,12 @@
 class HttpRequestError extends Error {
+
+    /*******************************************************************************
+    * @description Custom error class for http request errors.
+    *
+    * @param {string} message: Error message
+    * @param {string} status: Http response status message
+    * @param {string} statusCode: Http response status code (expected codes 400 - 599)
+    */
     constructor(message, status, statusCode) {
         super(message);
         this.status = status;
