bca6e537fb2ee6a6c57d048da8d54c36dc72c4b7
Sravani Kristem <skristem@salesforce.com>
2020-05-01 10:23:47 -0700

    added a keyword to enable crlps and created an empty page object file for crlps

---
+++ robot/Cumulus/resources/CustomizableRollupsPageObject.py
@@ -0,0 +1,6 @@
+from cumulusci.robotframework.pageobjects import BasePage
+from cumulusci.robotframework.pageobjects import pageobject
+from cumulusci.robotframework.utils import capture_screenshot_on_error
+from BaseObjects import BaseNPSPPage
+from NPSP import npsp_lex_locators
+
--- robot/Cumulus/resources/NPSPSettingsPageObject.py
+++ robot/Cumulus/resources/NPSPSettingsPageObject.py
@@ -132,7 +132,19 @@ class NPSPSettingsPage(BaseNPSPPage, BasePage):
                 self.wait_for_message("Advanced Mapping is enabled")
                 self.npsp.choose_frame("Nonprofit Success Pack Settings")
             else:
-                self.builtin.log("Advanced Mapping is already enabled")        
+                self.builtin.log("Advanced Mapping is already enabled")   
+
+    @capture_screenshot_on_error
+    def Enable_customizable_rollups_if_not_enabled(self):
+        """Checks if advanced mapping is Enabled and enables if not enabled"""
+        locator=npsp_lex_locators['id'].format("navigateCRLPs")
+        if self.npsp.check_if_element_exists(locator):
+            self.builtin.log("Customizable Rollups is already enabled")
+        else:
+            self.builtin.log("Customizable Rollups is not enabled by default, enabling it")
+            self.click_toggle_button("Customizable Rollups")
+            self.selenium.wait_until_page_contains_element(locator,timeout=60)
+            self.builtin.log("Customizable Rollups is enabled")                 
             
     def verify_gift_entry_is_not_enabled(self):
         """Verifies that gift entry is not enabled by default 
