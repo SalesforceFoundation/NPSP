923793304596b27cb4f3a820436e6880cff69450
d.fuller <d.fuller@salesforce.com>
2020-05-19 18:15:34 -0400

    add helper for making permissions check server call

---
+++ src/aura/BDI_ManageAdvancedMapping/BDI_ManageAdvancedMappingHelper.js
@@ -0,0 +1,46 @@
+({
+    isSysAdmin : function(component) {
+        let userId = 'asdfsdf';
+
+        let action = component.get('c.isAdminUser');
+        action.setParams({
+            'userId' : userId
+        });
+
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            const SUCCESS = 'SUCCESS';
+            const ERROR = 'ERROR';
+
+            if(state === SUCCESS) {
+                let isAdmin = response.getReturnValue();
+
+                if(isAdmin) {
+                    component.set('v.shouldRender', true);
+                } else {
+                    component.set('v.shouldRender', false);
+                }
+            } else if(state === ERROR) {
+                let errors = response.getError();
+
+                if(errors) {
+                    if(errors[0] && errors[0].message) {
+                        this.showToast('sticky', 'error', errors[0].message);
+                    }
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    showToast: function(mode, type, message) {
+        let toastEvent = $A.get('e.force:showToast');
+
+        toastEvent.setParams({
+            mode: mode,
+            message: message,
+            type: type
+        });
+        toastEvent.fire();
+    }
+});
