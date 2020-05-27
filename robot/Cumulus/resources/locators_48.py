f75974aa24aeea8d06d8334cb155c788a0b4fc40
Ratna <rjanjanam@hotmail.com>
2020-05-26 21:40:10 -0700

    Removing unwanted locators

--- robot/Cumulus/resources/locators_48.py
+++ robot/Cumulus/resources/locators_48.py
@@ -263,10 +263,6 @@ npsp_lex_locators={
         'new_button': '//button[text() = "{}"]',
         'select_locator': "//select[@name ='{}']",
         'success_toast': "//div[contains(@class,'slds-notify.slds-notify_toast,slds-theme_success')]",
-        'target_option': "//select[@name = 'Target Field']/option[text()='{}']",
-        'target': "//select[@name = 'Target Field']",
-        'target_object': "//select[@name = 'Target Object']",
-        'target_object_option': "//select[@name = 'Target Object']/option[text()='{}']",
         'active_setting_record': "//td[@data-label='Active']/following::tr/th//span/div//lightning-button/button[text()='{}']",
         'rollup_progress_notification': "//div/h2[contains(text(),'{}')]"
        },
