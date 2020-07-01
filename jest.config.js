fd8e8fc9b7681f5d559a9d1cbd4afeb1b0fe795d
Michael Smith <force2b@users.noreply.github.com>
2020-07-01 18:41:20 -0400

    Delete file accidentally committed

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
-module.exports = {
-    ...jestConfig,
-    // add any custom configurations here
-    moduleNameMapper: {
-       // '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button',
-    }
-};
