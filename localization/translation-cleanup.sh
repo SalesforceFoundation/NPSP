fb6dea98a7012e74dbc819e18389fd92f20f3bfa
Michael Smith <smithmichael@salesforce.com>
2020-07-23 10:15:42 -0400

    Remove the JQ dependency now that hasSampleData is set to false by default

--- localization/translation-cleanup.sh
+++ localization/translation-cleanup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+## Verify that hasSampleData is set to false before continuing
+if grep -q '"hasSampleData": true' orgs/feature.json
+then
+    echo "WARNING: The feature.json still has \"hasSampleData\" set to true. Cannot continue until this is set to false."
+    exit
+fi
+
 echo "###############################################################################################"
 echo "What this script does:"
-echo "1. Create a temporary Scratch Org definition that has the 'sampleData' attribute disabled"
-echo "2. Install all underlying package dependencies into the scratch org - using any available beta packages"
-echo "3. Deploy all unpackaged metadata to the scratch org"
-echo "4. Use the cci 'retrieve_unpackaged' command to retrieve all configuration from the org into the src folder"
-echo "5. Reset and/or delete any retrieved metadata that is unrelalated to translations"
-echo "6. Execute the cci 'cleanup_translation_metadata' task against the metadata to strip invalid or extranous xml content"
-echo "7. Update the RD2 unpackaged metadata translations to include special deprecated label overrides"
+echo "1. Install all underlying package dependencies into the scratch org - using any available beta packages"
+echo "2. Deploy all unpackaged metadata to the scratch org"
+echo "3. Use the cci 'retrieve_unpackaged' command to retrieve all configuration from the org into the src folder"
+echo "4. Reset and/or delete any retrieved metadata that is unrelalated to translations"
+echo "5. Execute the cci 'cleanup_translation_metadata' task against the metadata to strip invalid or extranous xml content"
+echo "6. Update the RD2 unpackaged metadata translations to include special deprecated label overrides"
 echo ""
 echo ""
 echo "When the script has fully completed, the developer should review any pending changes in an IDE, such as VSCode"
@@ -17,36 +23,21 @@ echo ""
 echo "ONLY changes to the 'objectTranslation' and 'translation' files should be commited to the repository."
 echo "ALL OTHER CHANGED FILES SHOULD BE IGNORED."
 echo "###############################################################################################"
-
 read -s -n 1 -p "Press any key to continue ..."
+echo ""
 
-## Verify that JQ is installed before continuing
-if ! command -v jq &> /dev/null
-then
-    echo "JQ is not installed. Use 'brew install jq' to install JQ from the command line."
-    exit
-fi
-
-ECHO "# === Create a temporary org definition file with sample data disabled"
-mv orgs/feature.json orgs/feature-save.json
-jq '.hasSampleData = false' orgs/feature-save.json > orgs/temp.json
-jq '.orgName = "NPSP - Translations"' orgs/temp.json > orgs/feature.json
-
-ECHO "# ==== CREATE THE SCRATCH ORG ===="
+echo "# ==== CREATE THE SCRATCH ORG ===="
 cci org remove translations
 cci org scratch feature translations --days 1
 cci flow run dependencies --org translations 
 cci flow run deploy_unmanaged --org translations
 cci task run update_admin_profile --org translations
 
-rm orgs/temp.json
-mv orgs/feature-save.json orgs/feature.json
-
-ECHO "# ==== RESET the Package.xml and use retrieve_unpackaged to pull down the metadata ===="
+echo "# ==== RESET the Package.xml and use retrieve_unpackaged to pull down the metadata ===="
 git checkout src/package.xml
 cci task run retrieve_unpackaged --org translations -o path src -o package_xml src/package.xml
 
-ECHO "# ==== DISCARD other files pulled down by the above task that we do not need ===="
+echo "# ==== DISCARD other files pulled down by the above task that we do not need ===="
 git checkout src/applications/*
 git checkout src/aura/*
 git checkout src/classes/*
@@ -65,7 +56,7 @@ git checkout src/triggers/*
 git checkout src/workflows/*
 git checkout src/package.xml
 
-ECHO "# ==== CLEANUP Empty Unpackaged ObjectTranslation Files ===="
+echo "# ==== CLEANUP Empty Unpackaged ObjectTranslation Files ===="
 
 rm src/objectTranslations/DuplicateRecordSet*.objectTranslation
 
@@ -82,10 +73,10 @@ rm unpackaged/config/trial/objectTranslations/___NAMESPACE___Trigger_Handler__c-
 
 rm unpackaged/post/first/objectTranslations/Global-*.objectTranslation
 
-ECHO "# ==== CLEANUP task to strip out extraneous elements from the translation files ===="
+echo "# ==== CLEANUP task to strip out extraneous elements from the translation files ===="
 cci task run cleanup_translation_metadata
 
-ECHO "# ==== Insert special translation overrides for Enhanced Recurring Donations unpackaged metadata ===="
+echo "# ==== Insert special translation overrides for Enhanced Recurring Donations unpackaged metadata ===="
 
 # The GREP command looks for the word "DEPRECATED" in the translation file.
 # If it is NOT found, then SED is used to insert the 5 override translations for the fields deprecated in RD2
@@ -127,4 +118,4 @@ fi
 
 cci org remove translations
 
-ECHO "# ==== DONE! - Review the remaining modified translation files before committing back into the branch"
+echo "# ==== DONE! - Review the remaining modified translation files before committing back into the branch"
