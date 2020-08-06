b0c7d6bfcefee27ff399d05a4a673fb66bc5ba22
Vladimir Martinov <vmartinov@vmartinov-ltm.internal.salesforce.com>
2020-08-06 16:50:09 -0700

    lightning file upload test

--- src/lwc/eocrGiftEntry/eocrGiftEntry.js
+++ src/lwc/eocrGiftEntry/eocrGiftEntry.js
@@ -3,16 +3,23 @@ import { LightningElement, api, wire, track } from 'lwc';
 export default class EocrGiftEntry extends LightningElement {
 
     @api readyToEnter = false;
-    checkBase64 = '';
-    deviceBase64 = '';
-    otherBase64 = '';
+    checkBase64;
+    deviceBase64;
+    otherBase64;
+
+    checkDocumentId;
+    deviceDocumentId;
+    otherDocumentId;
 
     enterGifts() {
         this.readyToEnter = true;
+        this.checkBase64 = '';
+        this.deviceBase64 = '';
+        this.otherBase64 = '';
     }
 
     done() {
-        this.uploadFiles();
+        this.submitFiles();
         this.readyToEnter = false;
     }
 
@@ -20,43 +27,103 @@ export default class EocrGiftEntry extends LightningElement {
         alert('done and new');
     }
 
-    handleUploadFinished(event) {
+    get acceptedFormats() {
+        return ['.png', '.jpeg', '.jpg'];
+    }
+
+    handleCheckUploadFinished(event) {
         const files = event.detail.files;
-        alert(JSON.stringify(files));
+        //alert(JSON.stringify(files));
+        let file = files[0];
+        alert(JSON.stringify(file));
+        this.checkDocumentId = file.documentId;
+    }
+
+    // handler for lightning-upload
+    // handleUploadFinished(event) {
+    //     const files = event.detail.files;
+    //     alert(JSON.stringify(files));
+    // }
+
+    clearFileUpload() {
+        this.value = null;
     }
 
     uploadCheck() {
+        alert('in uploadCheck');
+        const componentThis = this;
         const file = this.template.querySelector('[data-id="check-upload"]').files[0];
-        console.log(file);
 
         const reader = new FileReader();
         reader.addEventListener("load", function () {
             alert('in event listener');
             // convert image file to base64 string
             let r = reader.result;
-            let base64 = btoa(String.fromCharCode(...new Uint8Array(r)));
-            console.log(base64);
+            componentThis.checkBase64 = btoa(String.fromCharCode(...new Uint8Array(r)));
+            //console.log(componentThis.checkBase64);
+            alert(componentThis.checkBase64.substring(1, 10));
 
           }, false);
 
           if (file) {
-            //reader.readAsDataURL(file);
             reader.readAsArrayBuffer(file);
           }
     }
 
-    uploadFiles() {
-        alert('uploading files');
+    uploadDevice() {
+        const componentThis = this;
+        const file = this.template.querySelector('[data-id="device-upload"]').files[0];
 
-        // get the check
-        //const file = document.querySelector('input[type=file]').files[0];
-        //const file = document.querySelector('#check-upload').files[0];
-        alert(file);
-        //const reader = new FileReader();
-        //let r = reader.result;
+        const reader = new FileReader();
+        reader.addEventListener("load", function () {
+            // convert image file to base64 string
+            let r = reader.result;
+            componentThis.deviceBase64 = btoa(String.fromCharCode(...new Uint8Array(r)));
+            //console.log(componentThis.checkBase64);
 
-        //alert(r);
-              
+          }, false);
+
+          if (file) {
+            reader.readAsArrayBuffer(file);
+          }
+    }
+
+    uploadOther() {
+        const componentThis = this;
+        const file = this.template.querySelector('[data-id="other-upload"]').files[0];
+
+        const reader = new FileReader();
+        reader.addEventListener("load", function () {
+            // convert image file to base64 string
+            let r = reader.result;
+            componentThis.otherBase64 = btoa(String.fromCharCode(...new Uint8Array(r)));
+            //console.log(componentThis.checkBase64);
+
+          }, false);
+
+          if (file) {
+            reader.readAsArrayBuffer(file);
+          }
+    }
+
+    submitFiles() {
+        alert('submitting files');
+        alert(this.checkDocumentId);
+        //console.log('in submit = ' + this.checkBase64);
+        // if (0 < this.checkBase64.length) {
+        //     alert('check');
+        // }
+
+        // if (0 < this.deviceBase64.length) {
+        //     alert('device');
+        // }
+
+        // if (0 < this.otherBase64.length) {
+        //     alert('other');
+        // }
+        // alert(this.checkBase64.substring(1,5));
+        // alert(this.deviceBase64.substring(1, 5));
+        // alert(this.otherBase64.substring(1, 5));
        
     }
 
