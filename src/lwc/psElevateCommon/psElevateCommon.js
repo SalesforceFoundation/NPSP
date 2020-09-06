1f8b08e5797b74f7205421ccf1dee369d7537341
radSFDeveloper <raskraba@salesforce.com>
2020-09-06 14:58:19 -0700

    Extract common Elevate tokenization methods into the common class.

--- src/lwc/psElevateCommon/psElevateCommon.js
+++ src/lwc/psElevateCommon/psElevateCommon.js
@@ -1,4 +1,10 @@
 
+import { fireEvent } from 'c/pubsubNoPageRef';
+import { getNamespace, isFunction } from 'c/utilCommon';
+
+import PAYMENT_AUTHORIZATION_TOKEN_FIELD from '@salesforce/schema/DataImport__c.Payment_Authorization_Token__c';
+import tokenRequestTimedOut from '@salesforce/label/c.gePaymentRequestTimedOut';
+
 
 /***
 * @description Visualforce page for the credit card tokenization
@@ -6,39 +12,147 @@
 const TOKENIZE_CARD_PAGE_NAME = 'GE_TokenizeCard';
 
 /***
+* @description Event action sent to the Visualforce page to request the token
+*/
+const TOKENIZE_EVENT_ACTION = 'createToken';
+
+/***
 * @description Max number of ms to wait for the response
 */
 const TOKENIZE_TIMEOUT_MS = 10000;
 
-/***
-* @description Returns tokenize card Visualforce page URL
-*/
-const getTokenizeCardPageURL = (namespace) => {
-    return namespace
-        ? `/apex/${namespace}__${TOKENIZE_CARD_PAGE_NAME}`
-        : `/apex/${TOKENIZE_CARD_PAGE_NAME}`;
-}
 
-/***
-* @description Builds the visualforce origin url that we need in order to
-* make sure we're only listening for messages from the correct source in the
-* registerPostMessageListener method.
-*/
-const getVisualforceOriginURLs = (domainInfo, namespace) => {
-    let url = `https://${domainInfo.orgDomain}--c.visualforce.com`;
-    let alternateUrl = `https://${domainInfo.orgDomain}--c.${domainInfo.podName}.visual.force.com`;
+class PSElevateCommon {
+    /***
+    * @description Max number of ms to wait for the response
+    */
+    labels = Object.freeze({
+        tokenRequestTimedOut
+    });
 
-    if (namespace) {
-        url = url.replace('--c', `--${namespace}`);
-        alternateUrl = alternateUrl.replace('--c', `--${namespace}`);
+
+    /***
+    * @description Returns tokenize card Visualforce page URL
+    */
+    getTokenizeCardPageURL() {
+        const namespace = this.getCurrentNamespace();
+
+        return namespace
+            ? `/apex/${namespace}__${TOKENIZE_CARD_PAGE_NAME}`
+            : `/apex/${TOKENIZE_CARD_PAGE_NAME}`;
     }
 
-    return [{ value: url }, { value: alternateUrl }];
+    /***
+    * @description Builds the visualforce origin url that we need in order to
+    * make sure we're only listening for messages from the correct source in the
+    * registerPostMessageListener method.
+    */
+    getVisualforceOriginURLs(domainInfo) {
+        const namespace = this.getCurrentNamespace();
+
+        let url = `https://${domainInfo.orgDomain}--c.visualforce.com`;
+        let alternateUrl = `https://${domainInfo.orgDomain}--c.${domainInfo.podName}.visual.force.com`;
+
+        if (namespace) {
+            url = url.replace('--c', `--${namespace}`);
+            alternateUrl = alternateUrl.replace('--c', `--${namespace}`);
+        }
+
+        return [{ value: url }, { value: alternateUrl }];
+    }
+
+    /***
+    * @description
+    */
+    getCurrentNamespace() {
+        return getNamespace(PAYMENT_AUTHORIZATION_TOKEN_FIELD.fieldApiName);
+    }
+
+    /***
+    * @description
+    */
+    dispatchApplicationEvent(eventName, payload) {
+        fireEvent(null, eventName, payload);
+    }
+
+    /***
+    * @description Method listens for a message from the visualforce iframe.
+    * Rejects any messages from an unknown origin.
+    */
+    registerPostMessageListener(component) {
+        window.onmessage = async function (event) {
+            component.visualforceOrigin = component.visualforceOriginUrls.find(
+                origin => event.origin === origin.value
+            ).value;
+
+            if (component.visualforceOrigin) {
+                const message = JSON.parse(event.data);
+                component.handleMessage(message);
+            }
+        }
+    }
+
+    /***
+    * @description Method handles messages received from iframed visualforce page.
+    *
+    * @param {object} message: Message received from iframe
+    */
+    handleMessage(message) {
+        if (message.error || message.token) {
+            if (isFunction(this.tokenCallback)) {
+                this.tokenCallback(message);
+            }
+        } 
+    }
+
+    /***
+    * @description Method sends a message to the visualforce page iframe requesting
+    * a token. Response for this request is found and handled in
+    * registerPostMessageListener.
+    */
+    requestToken(visualforceOrigin, iframe, handleError) {
+        if (iframe) {
+            const tokenPromise = new Promise((resolve, reject) => {
+
+                const timer = setTimeout(() => 
+                    reject(this.handleTimeout(handleError)), 
+                    TOKENIZE_TIMEOUT_MS
+                );
+
+                this.tokenCallback = message => {
+                    clearTimeout(timer);
+
+                    if (message.error) {
+                        reject(handleError(message));
+
+                    } else if (message.token) {
+                        resolve(message.token);
+                    }
+                };
+
+            });
+
+            iframe.contentWindow.postMessage(
+                { action: TOKENIZE_EVENT_ACTION },
+                visualforceOrigin
+            );
+
+            return tokenPromise;
+        }
+    }
+
+    /**
+     *
+     */
+    handleTimeout(handleError) {
+        const message = {
+            error: this.labels.tokenRequestTimedOut,
+            isObject: false
+        };
+
+        handleError(message);
+    }
 }
 
 
-export {
-    TOKENIZE_TIMEOUT_MS,
-    getVisualforceOriginURLs,
-    getTokenizeCardPageURL,
-};
+export default new PSElevateCommon();
--- src/lwc/rd2ElevateCreditCardForm/rd2ElevateCreditCardForm.js
+++ src/lwc/rd2ElevateCreditCardForm/rd2ElevateCreditCardForm.js
@@ -1,18 +1,10 @@
 import { api, track, LightningElement } from 'lwc';
-import { fireEvent } from 'c/pubsubNoPageRef';
-import { getNamespace, isFunction } from 'c/utilCommon';
-import {
-    TOKENIZE_TIMEOUT_MS,
-    getTokenizeCardPageURL, getVisualforceOriginURLs
-} from 'c/psElevateCommon';
 
-import PAYMENT_AUTHORIZATION_TOKEN_FIELD from '@salesforce/schema/DataImport__c.Payment_Authorization_Token__c';
-
+import psElevateCommon from 'c/psElevateCommon';
 import getOrgDomainInfo from '@salesforce/apex/PS_ElevateController.getOrgDomainInfo';
 
 import commonPaymentServices from '@salesforce/label/c.commonPaymentServices';
 import assistiveSpinner from '@salesforce/label/c.geAssistiveSpinner';
-import tokenRequestTimedOut from '@salesforce/label/c.gePaymentRequestTimedOut';
 
 import rd2ElevateDisableLabel from '@salesforce/label/c.RD2_ElevateDisableLabel';
 import rd2ElevateDisabledMessage from '@salesforce/label/c.RD2_ElevateDisabledMessage';
@@ -25,36 +17,35 @@ export default class psElevateCreditCardForm extends LightningElement {
     labels = {
         commonPaymentServices,
         assistiveSpinner,
-        tokenRequestTimedOut,
         rd2ElevateDisableLabel,
         rd2ElevateDisabledMessage,
         rd2ElevateEnableLabel
     };
 
-
     @api cardHolderName;
 
-    @track domain;
     @track visualforceOrigin;
+    @track visualforceOriginUrls;
+
     @track isLoading = true;
-    @track alert = {};
     @track isDisabled = false;
-    _visualforceOriginUrls;
+    @track alert = {};
 
     /***
     * @description
     */
     async connectedCallback() {
-        this.domainInfo = await getOrgDomainInfo();
+        const domainInfo = await getOrgDomainInfo();
 
-        this._visualforceOriginUrls = getVisualforceOriginURLs(this.domainInfo, this.getCurrentNamespace());
+        this.visualforceOriginUrls = psElevateCommon.getVisualforceOriginURLs(domainInfo);
     }
 
     /***
     * @description
     */
     renderedCallback() {
-        this.registerPostMessageListener();
+        let component = this;
+        psElevateCommon.registerPostMessageListener(component);
     }
 
     /***
@@ -63,18 +54,12 @@ export default class psElevateCreditCardForm extends LightningElement {
     disconnectedCallback() {
     }
 
-    /***
-    * @description
-    */
-    getCurrentNamespace() {
-        return getNamespace(PAYMENT_AUTHORIZATION_TOKEN_FIELD.fieldApiName);
-    }
 
     /***
     * @description 
     */
     get tokenizeCardPageUrl() {
-        return getTokenizeCardPageURL(this.getCurrentNamespace());
+        return psElevateCommon.getTokenizeCardPageURL();
     }
 
     /***
@@ -82,7 +67,7 @@ export default class psElevateCreditCardForm extends LightningElement {
     */
     handleUserDisabledWidget() {
         this.hideWidget();
-        this.dispatchApplicationEvent(WIDGET_DISABLED_EVENT_NAME, {
+        psElevateCommon.dispatchApplicationEvent(WIDGET_DISABLED_EVENT_NAME, {
             isWidgetDisabled: true
         });
     }
@@ -93,7 +78,7 @@ export default class psElevateCreditCardForm extends LightningElement {
     handleUserEnabledWidget() {
         this.isLoading = true;
         this.displayWidget();
-        this.dispatchApplicationEvent(WIDGET_DISABLED_EVENT_NAME, {
+        psElevateCommon.dispatchApplicationEvent(WIDGET_DISABLED_EVENT_NAME, {
             isWidgetDisabled: false
         });
     }
@@ -115,43 +100,14 @@ export default class psElevateCreditCardForm extends LightningElement {
     }
 
     /***
-    * @description
-    */
-    dispatchApplicationEvent(eventName, payload) {
-        fireEvent(null, eventName, payload);
-    }
-
-    /***
-    * @description Method listens for a message from the visualforce iframe.
-    * Rejects any messages from an unknown origin.
-    */
-    registerPostMessageListener() {
-        let component = this;
-
-        window.onmessage = async function (event) {
-            component.visualforceOrigin = component._visualforceOriginUrls.find(
-                origin => event.origin === origin.value
-            ).value;
-
-            if (component.visualforceOrigin) {
-                const message = JSON.parse(event.data);
-                component.handleMessage(message);
-            }
-        }
-    }
-
-    /***
     * @description Method handles messages received from iframed visualforce page.
     *
     * @param {object} message: Message received from iframe
     */
     async handleMessage(message) {
-        if (message.error || message.token) {
-            if (isFunction(this.tokenCallback)) {
-                this.tokenCallback(message);
-            }
-
-        } else if (message.isLoaded) {
+        psElevateCommon.handleMessage(message);
+        
+        if (message.isLoaded) {
             this.isLoading = false;
         }
     }
@@ -164,44 +120,13 @@ export default class psElevateCreditCardForm extends LightningElement {
     requestToken() {
         const iframe = this.template.querySelector(`[data-id='${this.labels.commonPaymentServices}']`);
 
-        if (iframe) {
-            const tokenPromise = new Promise((resolve, reject) => {
-
-                const timer = setTimeout(() => reject(this.handleTokenizationTimeout()), TOKENIZE_TIMEOUT_MS);
-
-                this.tokenCallback = message => {
-                    clearTimeout(timer);
-                    this.clearError();
-
-                    if (message.error) {
-                        reject(this.handleTokenizationError(message));
-
-                    } else if (message.token) {
-                        resolve({
-                            [PAYMENT_AUTHORIZATION_TOKEN_FIELD.fieldApiName]: message.token
-                        });
-                    }
-                };
-
-            });
-
-            iframe.contentWindow.postMessage(
-                { action: 'createToken' },
-                this.visualforceOrigin
-            );
-
-            return tokenPromise;
-        }
+        return psElevateCommon.requestToken(this.visualforceOrigin, iframe, this.handleError);
     }
 
-    handleTokenizationTimeout() {
-        return {
-            error: this.labels.tokenRequestTimedOut,
-            isObject: false
-        };
-    }
-
-    handleTokenizationError(message) {
+    /**
+     *
+     */
+    handleError = (message) => {
         let errorValue;
         let isObject = false;
 
