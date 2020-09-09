4be9ec771e3ba0aba0173132a07dcaaed39395f0
d.fuller <d.fuller@salesforce.com>
2020-09-09 15:08:43 -0400

    Revert "pass data import source object info down through the template builder hierarchy"
    This reverts commit 8f4d0acd

--- src/lwc/geTemplateBuilderFormSection/geTemplateBuilderFormSection.js
+++ src/lwc/geTemplateBuilderFormSection/geTemplateBuilderFormSection.js
@@ -13,7 +13,6 @@ export default class GeTemplateBuilderFormSection extends LightningElement {
     @api activeFormSectionId;
     @api isFirst;
     @api isLast;
-    @api sourceObjectFieldInfo;
 
     @api formSection;
 
