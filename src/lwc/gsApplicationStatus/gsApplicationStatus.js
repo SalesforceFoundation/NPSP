d2d3d47a09364f48c0efcc0c37eb59caff859e0e
diego-caballero <diego-caballero@users.noreply.github.com>
2020-09-23 16:37:38 -0300

    [GETTING STARTED] Calculating days left of the trial in the org

--- src/lwc/gsApplicationStatus/gsApplicationStatus.js
+++ src/lwc/gsApplicationStatus/gsApplicationStatus.js
@@ -1,9 +1,30 @@
-import { LightningElement, track } from 'lwc';
-
+import { LightningElement, track, wire } from 'lwc';
+import getApplicationStatus from '@salesforce/apex/GS_ApplicationStatusController.getApplicationStatus'
 export default class NonProfit_GetProduct extends LightningElement {
-    @track penndig = false;
     
-    onGetFreeSldc(event) {
-        this.penndig = true;
+    @track errorMessage = "";
+    @track diffInDays = null;
+    connectedCallback() {
+        getApplicationStatus()
+        .then(result => {
+            console.info(result.isSandbox);
+            console.info(result.trialExpirationDate);
+
+            this.diffInDays = this.calculateDiffDays(new Date(result.trialExpirationDate));
+            
+        })
+        .catch(error => {
+            this.errorMessage = error;
+        });
+    }
+
+    calculateDiffDays(date) {
+        const oneDay = 24 * 60 * 60 * 1000
+
+        console.info("date", date);
+        
+        let today = new Date();
+        console.info("today", today);
+        return Math.ceil(Math.abs(date - today)/oneDay);
     }
 }
