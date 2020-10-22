52bc0cc8c4453c21bb1488b94e1a5d1023f98203
Felipe Parodi <fparodi@salesforce.com>
2020-10-22 12:41:18 -0300

    [GS] Fix Checklist After coder Review

--- src/lwc/gsAdminSetup/gsAdminSetup.js
+++ src/lwc/gsAdminSetup/gsAdminSetup.js
@@ -1,10 +1,11 @@
 import { LightningElement, track } from 'lwc';
-
 import gsAdminSetupTitle from '@salesforce/label/c.gsAdminSetupTitle';
-
 import data from './data';
 
 
+/*
+* @description This class control all the Admin Checklist information
+*/
 export default class gsAdminSetup extends LightningElement {
 
     @track checklists = data;
