c239acf7e022f1f8865f22313bc19d9ef5f58125
Felipe Parodi <fparodi@salesforce.com>
2020-10-28 17:11:45 -0300

    [GS] Add accordionSection componenet and reuse that

--- src/lwc/gsChecklist/gsChecklist.js
+++ src/lwc/gsChecklist/gsChecklist.js
@@ -12,47 +12,5 @@ export default class gsChecklist extends LightningElement {
     * @type      Checklist
     */
     @api group;
-    @track open = false;
-
-
-    /**
-    * @description Click event handler to display or not info section
-    * @return      void
-    * @see         Action
-    */
-    onClickOpenOrClose(event) {
-        this.open = !this.open;
-    }
     
-    /**
-    * @description This method return a expanded Id
-    * @return      String
-    */
-    get expandedId() {
-        return `section-${this.group ? this.group.id : '0'}`;
-    }
-
-    /**
-    * @description Return alter Text to arrow button
-    * @return      String
-    */
-    get arrowAlterText() {
-        return this.open ? gsChecklistArrowRightAlterText :  gsChecklistArrowDownAlterText;
-    }
-
-    /**
-    * @description Return icon to arrow button
-    * @return      String
-    */
-    get arrowIcon() {
-        return this.open ? 'utility:chevrondown' :  'utility:chevronright';
-    }
-
-    /**
-    * @description Return id to arrow button
-    * @return      String
-    */
-    get arrowId() {
-        return `arrow-${this.group ? this.group.id : '0'}`;
-    }
 }
