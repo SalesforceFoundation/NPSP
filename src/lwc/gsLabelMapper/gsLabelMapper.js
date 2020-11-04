a28c9f4e0bf15c1d02a0f58c435099b7e55ec6c5
Felipe Parodi <fparodi@salesforce.com>
2020-11-04 11:07:44 -0300

    [GS][W-7971251] Add Running Report

--- src/lwc/gsLabelMapper/gsLabelMapper.js
+++ src/lwc/gsLabelMapper/gsLabelMapper.js
@@ -18,6 +18,14 @@ import gsChecklistItemCreateDonationDesc from '@salesforce/label/c.gsChecklistIt
 import gsChecklistItemCreateDonationLinkLabel from '@salesforce/label/c.gsChecklistItemCreateDonationLinkLabel';
 import gsChecklistItemCreateDonationPriBtnLabel from '@salesforce/label/c.gsChecklistItemCreateDonationPriBtnLabel'; 
 import gsChecklistItemExtraOneMinuteVideo from '@salesforce/label/c.gsChecklistItemExtraOneMinuteVideo';
+import gsChecklistItemRunningReportsDesc from '@salesforce/label/c.gsChecklistItemRunningReportsDesc';
+import gsChecklistItemRunningReportsPriBtnLabel from '@salesforce/label/c.gsChecklistItemRunningReportsPriBtnLabel';
+import gsChecklistItemRunningReportsLinkLabel from '@salesforce/label/c.gsChecklistItemRunningReportsLinkLabel';
+import gsChecklistItemRunningReportsTitle from '@salesforce/label/c.gsChecklistItemRunningReportsTitle';
+gsChecklistItemRunningReportsDesc
+gsChecklistItemRunningReportsPriBtnLabel
+gsChecklistItemRunningReportsLinkLabel
+gsChecklistItemRunningReportsTitle
 
 /**
 * @description Map of getLabelValue labels
@@ -43,6 +51,10 @@ const labelMap = {
     gsChecklistItemCreateDonationLinkLabel,
     gsChecklistItemCreateDonationPriBtnLabel,
     gsChecklistItemExtraOneMinuteVideo,
+    gsChecklistItemRunningReportsDesc,
+    gsChecklistItemRunningReportsPriBtnLabel,
+    gsChecklistItemRunningReportsLinkLabel,
+    gsChecklistItemRunningReportsTitle,
 }
 
 /**
