e245acf55cfd96a2207c16e0d574953234c2cb9e
Ken Lewis <kenneth.lewis@salesforce.com>
2020-11-05 16:34:24 -0800

    Add description to flatten function

--- src/lwc/geFormRenderer/geFormRendererHelper.js
+++ src/lwc/geFormRenderer/geFormRendererHelper.js
@@ -1,3 +1,11 @@
+/**
+ * @description Helper function used to convert an object that has key value pairs where
+ * the value is an object with a value property, i.e. {value: {value:'', displayValue:''}},
+ * into an object that has primitives as values: {value: ''}.
+ * @param obj The object that has value objects.
+ * @returns An object that has primitives as values, derived from the "value" property of
+ * the fields on the passed in object.
+ */
 export function flatten(obj) {
     let flatObj = {};
     for (const [key, value] of Object.entries(obj)) {
