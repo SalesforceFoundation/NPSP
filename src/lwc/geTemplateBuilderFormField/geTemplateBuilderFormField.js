5f57a8903c7f612020ff18cd8b4c6d92d48bc589
Benjamin Dvorachek <bdvorachek@salesforce.com>
2020-12-03 11:09:21 -0600

    Revert "Fix default value checkbox appearing in template builder for checkbox to checkbox fields"
    This reverts commit 07c2cb965b861a7047b0ae9e4391ca721eab5359.

--- src/lwc/geTemplateBuilderFormField/geTemplateBuilderFormField.js
+++ src/lwc/geTemplateBuilderFormField/geTemplateBuilderFormField.js
@@ -27,7 +27,7 @@ export default class geTemplateBuilderFormField extends LightningElement {
     isBatchHeaderField = false;
     hasRendered = false;
     hasPermission = hasViewSetup;
-    _showDefaultValueInput = true;
+    shouldRender = true;
 
 
     /*******************************************************************************
@@ -83,7 +83,7 @@ export default class geTemplateBuilderFormField extends LightningElement {
 
             if (!hasViewSetup) {
                 this.field = {};
-                this.showDefaultValueInput = false;
+                this.shouldRender = false;
             }
 
             this.dispatchEvent(new CustomEvent(FIELD_METADATA_VALIDATION, {detail: {showError: true}}));
@@ -195,17 +195,6 @@ export default class geTemplateBuilderFormField extends LightningElement {
 
     get showRequiredCheckbox() {
         return !this.isWidget && !isCheckboxToCheckbox(this.fieldMapping);
-    }
-
-    get showDefaultValueInput() {
-        if(isCheckboxToCheckbox(this.fieldMapping)) {
-            return false;
-        }
-        return this._showDefaultValueInput;
-    }
-
-    set showDefaultValueInput(value) {
-        this._showDefaultValueInput;
     }
 
     get isTrueFalsePicklist() {
