9309db97733a86b1287de73ddbe103275ad7ef7d
dcaballeroSf <dcaballero@salesforce.com>
2021-01-05 17:48:01 -0300

    [END USER] Adding some documentation in the component

--- src/lwc/euTrailhead/euTrailhead.js
+++ src/lwc/euTrailhead/euTrailhead.js
@@ -3,14 +3,23 @@ import Resources from '@salesforce/resourceUrl/CumulusStaticResources'
 import euTrailheadText from '@salesforce/label/c.euTrailheadText'
 import euExploreTrailheadButton from '@salesforce/label/c.euExploreTrailheadButton'
 export default class EuTrailhead extends LightningElement {
-    
+    /**
+     * Url of the image shown in the component
+     */
     imgUrl = Resources + '/euAssets/trailheadLogo.png';
 
+    /**
+     * Object to reference the labels
+     */
     labels = {
         euTrailheadText,
         euExploreTrailheadButton
     }
 
+    /**
+     * Handles the click in the button. 
+     * It opens an url(https://sforce.co/2MyeNE9) in a new tab.
+     */
     handleClick() {
         window.open("https://sforce.co/2MyeNE9");
     }
