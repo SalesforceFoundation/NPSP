e718badf3afbdba31eeebfbaf81895761e689333
Andrew Yu <andrew.yu@salesforce.com>
2021-02-22 22:56:19 -0800

    payment modal skeleton

---
+++ src/lwc/rd2EditPaymentInformationModal/rd2EditPaymentInformationModal.js
@@ -0,0 +1,26 @@
+import { LightningElement, track } from 'lwc';
+import PaymentInformationTitle from '@salesforce/label/c.PaymentInformation';
+import cancelButtonLabel from '@salesforce/label/c.stgBtnCancel';
+import saveButtonLabel from '@salesforce/label/c.stgBtnSave';
+
+export default class Rd2EditCreditCardModal extends LightningElement {
+    @track nextDonationDate;
+
+
+
+    labels = Object.freeze({
+        PaymentInformationTitle,
+        cancelButtonLabel,
+        saveButtonLabel
+    });
+
+    handleClose() {
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+    handleClosedButtonTrapFocus() {
+
+    }
+    handleSaveButtonTrapFocus() {
+        
+    }
+}
