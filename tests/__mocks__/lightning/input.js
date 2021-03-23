d6b5ed727712ea4e7ff28c4f71663cc83b4acbfa
Benjamin Dvorachek <bdvorachek@salesforce.com>
2021-03-22 22:30:15 -0500

    Implement lightning-input mock w/ checkValidity jest function

---
+++ tests/__mocks__/lightning/input.js
@@ -0,0 +1,63 @@
+// see https://github.com/salesforce/sfdx-lwc-jest/blob/master/src/lightning-stubs/input/input.js
+
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { LightningElement, api } from 'lwc';
+export const mockCheckValidity = jest.fn();
+
+export default class Input extends LightningElement {
+    @api accept;
+    @api autocomplete;
+    @api checked;
+    @api dateAriaControls;
+    @api dateAriaDescribedBy;
+    @api dateAriaLabel;
+    @api dateAriaLabelledBy;
+    @api dateStyle;
+    @api disabled;
+    @api fieldLevelHelp;
+    @api files;
+    @api formatFractionDigits;
+    @api formatter;
+    @api isLoading;
+    @api label;
+    @api max;
+    @api maxLength;
+    @api messageToggleActive;
+    @api messageToggleInactive;
+    @api messageWhenBadInput;
+    @api messageWhenPatternMismatch;
+    @api messageWhenRangeOverflow;
+    @api messageWhenRangeUnderflow;
+    @api messageWhenStepMismatch;
+    @api messageWhenTooLong;
+    @api messageWhenTooShort;
+    @api messageWhenTypeMismatch;
+    @api messageWhenValueMissing;
+    @api min;
+    @api minLength;
+    @api multiple;
+    @api name;
+    @api pattern;
+    @api placeholder;
+    @api readOnly;
+    @api required;
+    @api step;
+    @api timeAriaControls;
+    @api timeAriaDescribedBy;
+    @api timeAriaLabelledBy;
+    @api timeStyle;
+    @api timezone;
+    @api type;
+    @api validity;
+    @api value;
+    @api variant;
+    @api checkValidity = mockCheckValidity;
+    @api reportValidity() {}
+    @api setCustomValidity() {}
+    @api showHelpMessageIfInvalid() {}
+}
