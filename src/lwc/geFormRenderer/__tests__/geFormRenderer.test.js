d106d977a92e6d963abd5871179a503b0770c74f
Benjamin Dvorachek <bdvorachek@salesforce.com>
2021-03-23 18:48:15 -0500

    WIP: donor names jest test

--- src/lwc/geFormRenderer/__tests__/geFormRenderer.test.js
+++ src/lwc/geFormRenderer/__tests__/geFormRenderer.test.js
@@ -4,6 +4,8 @@ import { mockCheckInputValidity } from 'lightning/input';
 import { mockCheckComboboxValidity } from 'lightning/combobox';
 import retrieveDefaultSGERenderWrapper from '@salesforce/apex/GE_GiftEntryController.retrieveDefaultSGERenderWrapper';
 import getPaymentTransactionStatusValues from '@salesforce/apex/GE_PaymentServices.getPaymentTransactionStatusValues';
+import sendPurchaseRequest from '@salesforce/apex/GE_GiftEntryController.sendPurchaseRequest';
+import GeLabelService from 'c/geLabelService';
 
 const mockWrapperWithNoNames = require('./data/retrieveDefaultSGERenderWrapper.json');
 const mockPaymentTransactionStatusValues = JSON.stringify(require('./data/paymentTransactionStatusValues.json'));
@@ -63,6 +65,72 @@ describe('c-ge-form-renderer', () => {
         getPaymentTransactionStatusValues.mockResolvedValue(mockPaymentTransactionStatusValues);
         retrieveDefaultSGERenderWrapper.mockResolvedValue(mockWrapperWithNoNames);
         // This error is specific to this mockRenderWrapperWithNoNames
+        // Donor Type, Donation Date, Donation Amount, Payment Method should appear as required
+
+        mockCheckInputValidity.mockReturnValue(true); // lightning-input is always valid
+        mockCheckComboboxValidity.mockReturnValue(true); // lightning-combobox is always valid
+
+        const element = createElement('c-ge-form-renderer', {is: GeFormRenderer});
+        document.body.appendChild(element);
+
+        const mockSubmit = jest.fn();
+        element.addEventListener('submit', mockSubmit);
+
+        return flushPromises().then(() => {
+            const btns = element.shadowRoot.querySelectorAll('lightning-button');
+            btns[1].click();
+            return flushPromises().then(() => {
+                expect(element).toMatchSnapshot();
+            });
+        });
+    });
+
+    it('form with payment widget, when payment method changed to credit card then payment iframe loads', () => {
+        getPaymentTransactionStatusValues.mockResolvedValue(mockPaymentTransactionStatusValues);
+        retrieveDefaultSGERenderWrapper.mockResolvedValue(mockWrapperWithNoNames);
+
+        mockCheckInputValidity.mockReturnValue(true); // lightning-input is always valid
+        mockCheckComboboxValidity.mockReturnValue(true); // lightning-combobox is always valid
+
+        const element = createElement('c-ge-form-renderer', { is: GeFormRenderer });
+        document.body.appendChild(element);
+
+        const mockSubmit = jest.fn();
+        element.addEventListener('submit', mockSubmit);
+
+        return flushPromises().then(() => {
+            const sections = element.shadowRoot.querySelectorAll('c-ge-form-section');
+            // for this template, the account1 billing city field is the second field in the second section
+
+            const sectionWithWidget = sections[0];
+            expect(sectionWithWidget.isPaymentWidgetAvailable).toBeTruthy(); // widget in first section
+
+            const paymentMethodEvent = new CustomEvent('formfieldchange', {
+                detail: {
+                    "value": "Credit Card",
+                    "label": "Credit Card",
+                    "fieldMappingDevName": "Payment_Method_87c012365"
+                }
+            });
+            sections[0].dispatchEvent(paymentMethodEvent);
+
+            return flushPromises().then(() => {
+
+                const { commonPaymentServices } = GeLabelService.CUSTOM_LABELS;
+                const iframeSelector = `iframe[data-id='${commonPaymentServices}']`;
+                const selectors = ['c-ge-form-widget', 'c-ge-form-widget-tokenize-card', iframeSelector];
+                const iframeElement = traverse(sectionWithWidget, ...selectors);
+                expect(iframeElement.tagName).toBe('iframe');
+                expect(iframeElement).toMatchSnapshot();
+            });
+        });
+    });
+
+
+    it('form without contact firstname when lookup populated and widget in chargeable state then sendPurchaseRequest is called with first name', () => {
+        getPaymentTransactionStatusValues.mockResolvedValue(mockPaymentTransactionStatusValues);
+        retrieveDefaultSGERenderWrapper.mockResolvedValue(mockWrapperWithNoNames);
+        // This error is specific to this mockRenderWrapperWithNoNames
         // Donor Type, Contact Preferred Email, Donation Date, Donation Amount, Payment Method appear as required
 
         mockCheckInputValidity.mockReturnValue(true); // lightning-input is always valid
@@ -70,39 +138,56 @@ describe('c-ge-form-renderer', () => {
 
         const element = createElement('c-ge-form-renderer', { is: GeFormRenderer });
         document.body.appendChild(element);
+
         const mockSubmit = jest.fn();
         element.addEventListener('submit', mockSubmit);
 
         return flushPromises().then(() => {
             const sections = element.shadowRoot.querySelectorAll('c-ge-form-section');
-            // for this template, the account1 billing city field is the second field in the second section
-            const account1BillingCity = sections[1].shadowRoot.querySelectorAll('c-ge-form-field')[1];
-            const changeEvent = new CustomEvent('formfieldchange', {
+
+            const sectionWithWidget = sections[0];
+            expect(sectionWithWidget.isPaymentWidgetAvailable).toBeTruthy(); // widget in first section
+
+            const contact1ChangeEvent = new CustomEvent('formfieldchange', {
                 detail: {
-                    value: 'Gary',
-                    label: 'Gary',
-                    fieldMappingDevName: 'Account1_City_a992c3bb1'
+                    "value": "003J000001zftFlIAI",
+                    "label": "003J000001zftFlIAI",
+                    "fieldMappingDevName": "Contact1Imported__c"
                 }
             });
-            account1BillingCity.dispatchEvent(changeEvent);
+            sections[0].dispatchEvent(contact1ChangeEvent);
+
             return flushPromises().then(() => {
-                const btns = element.shadowRoot.querySelectorAll('lightning-button');
-                btns[1].click();
-                return flushPromises().then(() => {
-                    expect(element).toMatchSnapshot();
-                })
+
+                const { commonPaymentServices } = GeLabelService.CUSTOM_LABELS;
+                const iframeSelector = `iframe[data-id='${commonPaymentServices}']`;
+
+                const iframeElement = traverse(sectionWithWidget,
+                    'c-ge-form-widget',
+                    'c-ge-form-widget-tokenize-card',
+                    iframeSelector
+                );
+
+                //expect(iframeElement.tagName).toBe('iframe');
+                //expect(iframeElement).toBeTruthy();
             });
         });
     });
+});
 
-    it('determines donor name when name fields are not in template', () => {
+const getFieldByIndex = (rendererElement, sectionIndex, fieldIndex) => {
+    const sections = rendererElement.shadowRoot.querySelectorAll('c-ge-form-section');
+    return sections[sectionIndex].shadowRoot.querySelectorAll('c-ge-form-field')[fieldIndex];
+}
 
-    });
-
-    it('shows an error when currencies are mismatched', () => {
-
-    });
-})
+const traverse = (element, ...selectors) => {
+    if(selectors.length === 1) {
+        return element.shadowRoot.querySelector(selectors[0]);
+    }
+    const [firstSelector, ...rest] = selectors;
+    const nextElement = element.shadowRoot.querySelector(firstSelector);
+    return traverse(nextElement, ...rest);
+}
 
 
 
