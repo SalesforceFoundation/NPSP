57f0fdc8958bdcdc6146788abba50b42168a611a
Benjamin Dvorachek <bdvorachek@salesforce.com>
2021-03-22 22:31:00 -0500

    WIP: Utilize mockCheckValidity in geFormRenderer as part of save flow test

--- src/lwc/geFormRenderer/__tests__/geFormRenderer.test.js
+++ src/lwc/geFormRenderer/__tests__/geFormRenderer.test.js
@@ -1,5 +1,6 @@
 import { createElement } from 'lwc';
 import GeFormRenderer from 'c/geFormRenderer';
+import { mockCheckValidity } from 'lightning/input';
 import retrieveDefaultSGERenderWrapper from '@salesforce/apex/GE_GiftEntryController.retrieveDefaultSGERenderWrapper';
 import getPaymentTransactionStatusValues from '@salesforce/apex/GE_PaymentServices.getPaymentTransactionStatusValues';
 
@@ -21,14 +22,6 @@ describe('c-ge-form-renderer', () => {
         jest.clearAllMocks();
     });
 
-    it('loads with nothing', () => {
-        const element = createElement('c-ge-form-renderer', { is: GeFormRenderer });
-        document.body.appendChild(element);
-        return flushPromises().then((() => {
-            expect(element).toMatchSnapshot();
-        }));
-    });
-
     it('loads with template', () => {
         getPaymentTransactionStatusValues.mockResolvedValue(mockPaymentTransactionStatusValues);
         retrieveDefaultSGERenderWrapper.mockResolvedValue(mockWrapperWithNoNames);
@@ -42,6 +35,65 @@ describe('c-ge-form-renderer', () => {
         });
     });
 
+    it('updates form state when a field is changed', () => {
+        getPaymentTransactionStatusValues.mockResolvedValue(mockPaymentTransactionStatusValues);
+        retrieveDefaultSGERenderWrapper.mockResolvedValue(mockWrapperWithNoNames);
+        const element = createElement('c-ge-form-renderer', { is: GeFormRenderer });
+        document.body.appendChild(element);
+        return flushPromises().then(() => {
+            const sections = element.shadowRoot.querySelectorAll('c-ge-form-section');
+            const account1BillingCity = sections[1].shadowRoot.querySelectorAll('c-ge-form-field')[1];
+            const changeEvent = new CustomEvent('formfieldchange', {
+                detail: {
+                    value: 'Gary',
+                    label: 'Gary',
+                    fieldMappingDevName: 'Account1_City_a992c3bb1'
+                }
+            });
+            account1BillingCity.dispatchEvent(changeEvent);
+            return flushPromises().then(() => {
+
+            });
+        });
+    });
+
+    it('does not save because of an error with an element', () => {
+        getPaymentTransactionStatusValues.mockResolvedValue(mockPaymentTransactionStatusValues);
+        retrieveDefaultSGERenderWrapper.mockResolvedValue(mockWrapperWithNoNames);
+        mockCheckValidity.mockReturnValue(true);
+        const element = createElement('c-ge-form-renderer', { is: GeFormRenderer });
+        document.body.appendChild(element);
+        const mockSubmit = jest.fn();
+        element.addEventListener('submit', mockSubmit);
+
+        return flushPromises().then(() => {
+            const sections = element.shadowRoot.querySelectorAll('c-ge-form-section');
+            const account1BillingCity = sections[1].shadowRoot.querySelectorAll('c-ge-form-field')[1];
+            const changeEvent = new CustomEvent('formfieldchange', {
+                detail: {
+                    value: 'Gary',
+                    label: 'Gary',
+                    fieldMappingDevName: 'Account1_City_a992c3bb1'
+                }
+            });
+            account1BillingCity.dispatchEvent(changeEvent);
+            return flushPromises().then(() => {
+                const btns = element.shadowRoot.querySelectorAll('lightning-button');
+                btns[1].click();
+                return flushPromises().then(() => {
+                    expect(element).toMatchSnapshot();
+                })
+            });
+        });
+    });
+
+    it('determines donor name when name fields are not in template', () => {
+
+    });
+
+    it('shows an error when currencies are mismatched', () => {
+
+    });
 })
 
 
