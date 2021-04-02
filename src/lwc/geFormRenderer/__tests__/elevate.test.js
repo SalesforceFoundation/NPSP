ba1dc5c05891443d0d8f4f7204bd0ea49da75b0f
Benjamin Dvorachek <bdvorachek@salesforce.com>
2021-04-02 16:06:20 -0500

    elevate service tests

---
+++ src/lwc/geFormRenderer/__tests__/elevate.test.js
@@ -0,0 +1,76 @@
+import apexAddToCaptureGroup from '@salesforce/apex/GE_GiftEntryController.addToCaptureGroup';
+import apexCreateCaptureGroup from '@salesforce/apex/GE_GiftEntryController.createCaptureGroup';
+
+jest.mock('@salesforce/apex/GE_GiftEntryController.addToCaptureGroup',
+    () => ({ default : jest.fn() }),
+    { virtual: true }
+);
+jest.mock('@salesforce/apex/GE_GiftEntryController.createCaptureGroup',
+    () => ({ default : jest.fn() }),
+    { virtual: true }
+);
+
+import ElevateCaptureGroup from "../elevateCaptureGroup";
+import ElevateTokenizeableGift from "../elevateTokenizeableGift";
+
+describe('elevate-capture-group', () => {
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('create capture group returns id of a new capture group', async () => {
+        apexCreateCaptureGroup.mockResolvedValue({
+            "groupId" : "fake-capture-group-id"
+        });
+
+        const captureGroup = new ElevateCaptureGroup();
+        const elevateBatchId = await captureGroup.create();
+
+        expect(elevateBatchId).toBe('fake-capture-group-id');
+    });
+
+    it('new capture group without id when adding gift then group has id', async () => {
+        apexCreateCaptureGroup.mockResolvedValue({
+            "groupId" : "fake-capture-group-id"
+        });
+
+        const tokenizableGift = getDummyGift();
+
+        const captureGroup = new ElevateCaptureGroup();
+        await captureGroup.add(tokenizableGift);
+
+        expect(apexAddToCaptureGroup).toHaveBeenLastCalledWith({
+            "groupId": "fake-capture-group-id",
+            tokenizedGift: tokenizableGift
+        });
+    });
+
+    it('capture group with existing id when adding gift then id is unchanged', async () => {
+        const captureGroup = new ElevateCaptureGroup('fakeId');
+
+        const tokenizableGift = getDummyGift();
+
+        const authorizedGift = await captureGroup.add(tokenizableGift);
+
+        expect(apexAddToCaptureGroup).toHaveBeenCalledTimes(1);
+        expect(apexCreateCaptureGroup).toHaveBeenCalledTimes(0);
+    });
+});
+
+
+const getDummyGift = () => {
+    return new ElevateTokenizeableGift(
+        'DummyFirstName',
+        'DummyLastName',
+        5.00,
+        'USD'
+    );
+}
+
+describe('elevate-tokenizable-gift', () => {
+    afterEach(() => {
+        clearDOM();
+        jest.clearAllMocks();
+    });
+});
