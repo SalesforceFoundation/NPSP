656bfd893cec981797733a0373156a2983940f86
Patrick Tewson <ptewson@salesforce.com>
2021-04-20 23:32:12 -0700

    Use wire service to populate donor names as string

--- src/lwc/oppDonationAttribution/oppDonationAttribution.js
+++ src/lwc/oppDonationAttribution/oppDonationAttribution.js
@@ -1,4 +1,4 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import getHardCreditDonorsFor 
     from '@salesforce/apex/DonorService.getHardCreditDonorsFor';
 
@@ -8,16 +8,33 @@ export default class OppDonationAttribution extends LightningElement {
     @api recordId;
     
     @track donors = []; 
+    donorNames = '';
+    error;
 
+    @wire(getHardCreditDonorsFor, { opportunityId: '$recordId' }) 
+    wiredDonors({data, error}) {
+        if (data) {
+            this.donorNames = data;
+        } else if (error) {
+            this.error = 'Unknown error';
+            if (Array.isArray(error.body)) {
+                this.error = error.body.map(e => e.message).join(', ');
+            } else if (typeof error.body.message === 'string') {
+                this.error = error.body.message;
+            }
+        }
+    };
+
+    /***
     async connectedCallback() {
-        this.donors = await getHardCreditDonorsFor(this.recordId);
+        this.donorNames = await getHardCreditDonorsFor({ recordId: this.recordId });
     }
-
     get displayText() {
-        if(this.donors.length > 0) {
-            return this.donors[0].fullName;
+        if(this.donorNames) {
+            return this.donorNames;
         } else {
             return 'Not Found';
         }
     }
+    */
 }
