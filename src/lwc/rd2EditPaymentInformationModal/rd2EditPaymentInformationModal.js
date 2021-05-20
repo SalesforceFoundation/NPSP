75cab7ae59bba8aaf46f47b5a0339b8ba4a61dd9
Benjamin Dvorachek <bdvorachek@salesforce.com>
2021-05-20 11:18:58 -0500

    Use isElevatePaymentMethod to filter available payment options in rd2EditPaymentInformationModal

--- src/lwc/rd2EditPaymentInformationModal/rd2EditPaymentInformationModal.js
+++ src/lwc/rd2EditPaymentInformationModal/rd2EditPaymentInformationModal.js
@@ -58,7 +58,7 @@ export default class rd2EditPaymentInformationModal extends LightningElement {
     @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: FIELD_PAYMENT_METHOD } )
     wiredPicklistValues({data}) {
         if(data) {
-            this.paymentMethodOptions = data.values.filter(({value}) => value === 'ACH' || value === 'Credit Card');
+            this.paymentMethodOptions = data.values.filter(({value}) => this.rd2Service.isElevatePaymentMethod(value));
         }
     }
 
