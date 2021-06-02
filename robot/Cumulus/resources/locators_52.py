91c6ed0f6769ab996d53fd50997699a271cd75b8
Matt Schlosser <mschlosser@salesforce.com>
2021-06-02 17:10:07 -0400

    Added locators for upcoming changes

--- robot/Cumulus/resources/locators_52.py
+++ robot/Cumulus/resources/locators_52.py
@@ -340,7 +340,9 @@ npsp_lex_locators={
 		'batch_error':'//div[@role="alert" and contains(@class,"slds-theme--error")]//div[text()="{}"]/following-sibling::span[text()="{}"]',
 		'close_error':'//div[@role="alert" and contains(@class,"slds-theme--error")]//child::button',
 		'perms_error':'//div[@class="slds-illustration slds-illustration_large"]',
-		'template_required_checkbox':'//lightning-input[@data-qa-locator="checkbox Required {}"]'
+		'template_required_checkbox':'//lightning-input[@data-qa-locator="checkbox Required {}"]',
+		'success_toast':'//span[contains(text(),"{} was processed.")]',
+        'collapse_header':'//span[contains(@title,"Gift Entry Form")]'
 	},
 	# Enhanced Recurring Donation (erd) related element locators
 	'erd':{
