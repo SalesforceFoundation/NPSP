30634cb3b04a70258e791f4c7ecd00d213b7550f
Victor Oduyemi <voduyemi@salesforce.com>
2021-06-02 15:10:39 -0600

    Fixed issue with SGE not loading

--- src/lwc/geGiftEntryFormApp/__tests__/geGiftEntryFormApp.test.js
+++ src/lwc/geGiftEntryFormApp/__tests__/geGiftEntryFormApp.test.js
@@ -2,6 +2,7 @@ import { createElement } from 'lwc';
 import GeGiftEntryFormApp from 'c/geGiftEntryFormApp';
 import { getRecord } from "@salesforce/sfdx-lwc-jest/src/lightning-stubs/uiRecordApi/uiRecordApi";
 import getGiftBatchTotalsBy from '@salesforce/apex/GE_GiftEntryController.getGiftBatchTotalsBy';
+import DATA_IMPORT_BATCH_OBJECT from '@salesforce/schema/DataImportBatch__c';
 const mockGetRecord = require('./data/getRecord.json');
 const PROCESSING_BATCH_MESSAGE = 'c.geProcessingBatch';
 const batchTotalsProcessingGifts = require('./data/batchTotalsProcessingGifts.json');
@@ -32,6 +33,7 @@ describe('c-ge-gift-entry-form-app', () => {
     it('should render processing batch spinner if batch is still processing', async  () => {
         getGiftBatchTotalsBy.mockResolvedValue(batchTotalsProcessingGifts);
         const formApp = createGeGiftEntryFormApp();
+        formApp.sObjectName = DATA_IMPORT_BATCH_OBJECT.objectApiName;
 
         document.body.appendChild(formApp);
         getRecord.emit(mockGetRecord);
