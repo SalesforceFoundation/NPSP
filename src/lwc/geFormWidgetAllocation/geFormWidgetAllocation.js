514a4c1382849234d1fd87773778440562b9af98
Patrick Tewson <ptewson@salesforce.com>
2021-06-10 17:16:56 -0700

    Correct timing of over-allocation validation

--- src/lwc/geFormWidgetAllocation/geFormWidgetAllocation.js
+++ src/lwc/geFormWidgetAllocation/geFormWidgetAllocation.js
@@ -87,6 +87,7 @@ export default class GeFormWidgetAllocation extends LightningElement {
             })
         });
         this.addRows(rowList);
+        this.validate();
     }
 
     get totalAmount() {
