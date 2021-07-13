defd1b2908f828c4f41a51739f9d14379efe7224
Patrick Tewson <ptewson@salesforce.com>
2021-07-13 11:26:41 -0700

    clean up populating soft credits in gift

--- src/lwc/geGiftEntryFormApp/helpers/gift.js
+++ src/lwc/geGiftEntryFormApp/helpers/gift.js
@@ -8,12 +8,7 @@ class Gift {
         
         softCreditOppContactRoles.forEach( oppContactRole => {
             let softCredit = { ...oppContactRole };
-            /***
-            softCredit.Role = oppContactRole.Role;
-            softCredit.ContactId = oppContactRole.ContactId;
-            softCredit.OpportunityId = oppContactRole.OpportunityId;
-            softCredit.Id = oppContactRole.Id;
-            **/
+            
             softCredit.isReadOnly = true;
             this.softCredits.push( softCredit );
         });        
