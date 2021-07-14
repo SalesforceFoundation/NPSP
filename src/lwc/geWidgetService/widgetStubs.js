fa78d2bc2d2c46a010c233d0ddacf920c3724ca2
Patrick Tewson <ptewson@salesforce.com>
2021-07-14 12:05:21 -0700

    Fix broken references to OCR fields

--- src/lwc/geWidgetService/widgetStubs.js
+++ src/lwc/geWidgetService/widgetStubs.js
@@ -13,13 +13,13 @@ const ALLOCATION_WIDGET_DEVELOPER_NAME_STUBS = {
 
 /*******************************************************************************
 * @description Stubs of object and field mapping developer names. Used to collect
-* the org specific mapping developer names for the widget geFormWidgetSoftCredits.
+* the org specific mapping developer names for the widget geFormWidgetSoftCredits. 
 */
 const SOFT_CREDIT_WIDGET_DEVELOPER_NAME_STUBS = {
     objectMappingDeveloperNameStubs: 'Opportunity_Contact_Role_1_',
     fieldMappingDeveloperNameStubs: [
-        'Opportunity_Contact_Role_1_Role_',
-        'Opportunity_Contact_Role_1_Contact_'
+        'Opportunity_Conct_Role_1_Role_',
+        'Opportunity_ConRole_1_Contact_'
     ]
 }
 
