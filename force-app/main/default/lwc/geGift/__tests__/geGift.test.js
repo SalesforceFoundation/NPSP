03a8aeb7cdec8e688c80b2b7244a9a1b5d55adf2
Prince Sadie <psadie@salesforce.com>
2021-07-25 17:42:02 -0700

    update geGift jest test

--- force-app/main/default/lwc/geGift/__tests__/geGift.test.js
+++ force-app/main/default/lwc/geGift/__tests__/geGift.test.js
@@ -14,7 +14,7 @@ describe('ge-gift', () => {
 
         await flushPromises();
 
-        expect(gift.fields).toEqual({"Contact1Imported__c": "0032F00000iz5YhQAI", "Contact1Imported__r": {"Id": "0032F00000iz5YhQAI", "Name": "Gloria Howell"}, "Contact1_Firstname__c": "Gloria", "Contact1_Lastname__c": "Howell", "Contact1_Preferred_Email__c": "Personal", "DonationImportStatus__c": "Dry Run - Matched None", "Donation_Amount__c": 55, "Donation_Date__c": "2021-07-23", "Donation_Donor__c": "Contact1", "Id": "a0U2F000002HwNrUAK", "NPSP_Data_Import_Batch__c": "a0T2F00000307TAUAY", "NPSP_Data_Import_Batch__r": {"Id": "a0T2F00000307TAUAY", "Name": "Test Batch 0"}, "Payment_Method__c": "Check", "Status__c": "Dry Run - Validated"});
-        expect(gift.softCredits.all.length).toBe(0);
+        expect(gift.state().fields).toEqual({"Contact1Imported__c": "0032F00000iz5YhQAI", "Contact1Imported__r": {"Id": "0032F00000iz5YhQAI", "Name": "Gloria Howell"}, "Contact1_Firstname__c": "Gloria", "Contact1_Lastname__c": "Howell", "Contact1_Preferred_Email__c": "Personal", "DonationImportStatus__c": "Dry Run - Matched None", "Donation_Amount__c": 55, "Donation_Date__c": "2021-07-23", "Donation_Donor__c": "Contact1", "Id": "a0U2F000002HwNrUAK", "NPSP_Data_Import_Batch__c": "a0T2F00000307TAUAY", "NPSP_Data_Import_Batch__r": {"Id": "a0T2F00000307TAUAY", "Name": "Test Batch 0"}, "Payment_Method__c": "Check", "Status__c": "Dry Run - Validated"});
+        expect(gift.state().softCredits.all.length).toBe(0);
     });
 });
