d7d701cbcfe6d9ac34dfba2c7ab7e3cf7b483f9b
Benjamin Dvorachek <bdvorachek@salesforce.com>
2021-08-05 14:31:57 -0500

    Add AuraEnabled to controller method, set up connectedCallback in changeHistory component

--- force-app/main/default/lwc/rd2ChangeHistory/rd2ChangeHistory.js
+++ force-app/main/default/lwc/rd2ChangeHistory/rd2ChangeHistory.js
@@ -1,7 +1,22 @@
-import { LightningElement, api } from 'lwc';
-
+import { LightningElement, api, track } from 'lwc';
+import getChangeHistory from '@salesforce/apex/RD2_ChangeHistoryController.getChangeHistory'
 export default class Rd2ChangeHistory extends LightningElement {
     @api recordId;
     @api changeTypeFilter;
     @api recordLimit;
+
+    @track changeHistory;
+
+    errorMessage;
+    hasError = false;
+
+    async connectedCallback() {
+        const { recordId, changeTypeFilter, recordLimit } = this;
+        try {
+            this.changeHistory = await getChangeHistory({recordId, changeTypeFilter, recordLimit});
+        } catch (ex) {
+            this.hasError = true;
+            this.errorMessage = ex.message;
+        }
+    }
 }
