c37a5c3221e1334973c7dbe9245ea78cb0e04d09
Benjamin Dvorachek <bdvorachek@salesforce.com>
2021-08-09 11:25:25 -0500

    View More link in Change History Component footer slot, states, labels

--- force-app/main/default/lwc/rd2ChangeHistory/__tests__/rd2ChangeHistory.test.js
+++ force-app/main/default/lwc/rd2ChangeHistory/__tests__/rd2ChangeHistory.test.js
@@ -2,6 +2,8 @@ import { createElement } from 'lwc';
 import Rd2ChangeHistory from 'c/rd2ChangeHistory';
 import getChangeHistory from '@salesforce/apex/RD2_ChangeHistoryController.getChangeHistory';
 import commonViewMore from '@salesforce/label/c.commonViewMore';
+import commonError from '@salesforce/label/c.commonError';
+import rdchNoRecords from '@salesforce/label/c.RDCH_No_Records';
 
 import { getNavigateCalledWith } from "lightning/navigation";
 
@@ -26,7 +28,6 @@ describe('c-rd2-change-entry-item', () => {
 
         const aTag = component.shadowRoot.querySelector('a');
         expect(aTag.textContent).toBe(commonViewMore);
-
     });
 
     it('view more link onclick navigates to related list', async () => {
@@ -46,7 +47,33 @@ describe('c-rd2-change-entry-item', () => {
         expect(pageReference.attributes.objectApiName).toBe('npe03__Recurring_Donation__c');
         expect(pageReference.attributes.relationshipApiName).toBe('RDChangeHistory__r');
         expect(pageReference.attributes.actionName).toBe('view');
-    })
+    });
+
+    it('when change history setting is disabled, renders an error state', async () => {
+        const component = createElement('c-rd2-change-entry-item', { is: Rd2ChangeHistory });
+        mockChangeHistoryView.settingEnabled = false;
+        getChangeHistory.mockResolvedValue(mockChangeHistoryView);
+        document.body.appendChild(component);
+        await flushPromises();
+
+        const illustration = component.shadowRoot.querySelector('c-util-illustration');
+        expect(illustration).toBeTruthy();
+    });
+
+    it('when no records are present, renders empty list state', async () => {
+        const component = createElement('c-rd2-change-entry-item', { is: Rd2ChangeHistory });
+        mockChangeHistoryView.settingEnabled = true;
+        mockChangeHistoryView.changes = [];
+
+        getChangeHistory.mockResolvedValue(mockChangeHistoryView);
+        document.body.appendChild(component);
+        await flushPromises();
+
+        const illustration = component.shadowRoot.querySelector('c-util-illustration');
+        expect(illustration).toBeTruthy();
+        expect(illustration.message).toBe(rdchNoRecords);
+    });
+
 });
 
 
--- force-app/main/default/lwc/rd2ChangeHistory/rd2ChangeHistory.js
+++ force-app/main/default/lwc/rd2ChangeHistory/rd2ChangeHistory.js
@@ -1,7 +1,20 @@
-import { LightningElement, api, track } from 'lwc';
-import commonViewMore from '@salesforce/label/c.commonViewMore';
+import { LightningElement, api, track, wire } from 'lwc';
 import getChangeHistory from '@salesforce/apex/RD2_ChangeHistoryController.getChangeHistory'
 import { NavigationMixin } from 'lightning/navigation';
+
+import commonViewMore from '@salesforce/label/c.commonViewMore';
+import commonError from '@salesforce/label/c.commonError';
+import rdchDisabled from '@salesforce/label/c.RDCH_Disabled';
+import rdchChangeHistory from '@salesforce/label/c.RDCH_Change_History';
+import rdchNoRecords from '@salesforce/label/c.RDCH_No_Records';
+
+const STATES = {
+    LOADING: 'LOADING',
+    READY: 'READY',
+    ERROR: 'ERROR',
+    SETTING_DISABLED: 'SETTING_DISABLED',
+    EMPTY_LIST: 'EMPTY_LIST'
+};
 
 export default class Rd2ChangeHistory extends NavigationMixin(LightningElement) {
     @api recordId;
@@ -10,21 +23,54 @@ export default class Rd2ChangeHistory extends NavigationMixin(LightningElement) 
 
     @track changeHistory;
 
+    displayState = STATES.LOADING;
     errorMessage;
-    isReady = false;
-    hasError = false;
 
-    labels = { commonViewMore };
+    labels = {
+        commonError,
+        commonViewMore,
+        rdchDisabled,
+        rdchChangeHistory,
+        rdchNoRecords
+    };
 
     async connectedCallback() {
         const { recordId, changeTypeFilter, recordLimit } = this;
         try {
-            this.changeHistory = await getChangeHistory({recordId, changeTypeFilter, recordLimit});
-            this.isReady = true;
+            this.changeHistory = await getChangeHistory({ recurringDonationId: recordId, changeTypeFilter, recordLimit });
+            this.displayState = this.changeHistory.settingEnabled ? STATES.READY : STATES.SETTING_DISABLED;
         } catch (ex) {
-            this.hasError = true;
+            this.displayState = STATES.ERROR;
             this.errorMessage = ex.message;
         }
+    }
+
+    get isSettingDisabled() {
+        return this.displayState === STATES.SETTING_DISABLED;
+    }
+
+    get hasChanges() {
+        return this.displayState === STATES.READY
+            && this.changeHistory.changes.length > 0
+            && this.changeHistory.hasMore === false;
+    }
+
+    get hasMoreChanges() {
+        return this.displayState === STATES.READY
+            && this.changeHistory.changes.length > 0
+            && this.changeHistory.hasMore === true;
+    }
+
+    get hasEmptyList() {
+        return this.displayState === STATES.READY && this.changeHistory.changes.length === 0;
+    }
+
+    get isLoading() {
+        return this.displayState === STATES.LOADING;
+    }
+
+    get hasError() {
+        return this.displayState === STATES.ERROR;
     }
 
     navigateToRelatedList() {
