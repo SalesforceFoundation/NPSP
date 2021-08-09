41658b7cc4b9ac360472a7c2980877b8ff1dbd88
Benjamin Dvorachek <bdvorachek@salesforce.com>
2021-08-09 13:21:25 -0500

    Split navigation / rendering of lookups into separate tests

--- force-app/main/default/lwc/rd2ChangeEntryValue/__tests__/rd2ChangeEntryValue.test.js
+++ force-app/main/default/lwc/rd2ChangeEntryValue/__tests__/rd2ChangeEntryValue.test.js
@@ -42,29 +42,32 @@ describe('c-rd2-change-entry-value', () => {
         expect(outputCmp.value).toBe(50);
     });
 
-    it('renders lookup fields', () => {
-        const component = createElement('c-rd2-change-entry-value', { is: Rd2ChangeEntryValue });
-        component.value = 'Some Record Name';
-        component.recordIdValue = 'fake_record_id';
-        component.displayType = 'LOOKUP';
-        document.body.appendChild(component);
+    describe('lookup fields', () => {
+        let component;
 
-        const outputCmp = component.shadowRoot.querySelector('a');
+        it('renders lookup fields', () => {
+            component = createElement('c-rd2-change-entry-value', { is: Rd2ChangeEntryValue });
+            component.value = 'Some Record Name';
+            component.recordIdValue = 'fake_record_id';
+            component.displayType = 'LOOKUP';
+            document.body.appendChild(component);
 
-        expect(outputCmp.textContent).toBe('Some Record Name');
-        outputCmp.click();
+            const outputCmp = component.shadowRoot.querySelector('a');
 
-        const { pageReference } = getNavigateCalledWith();
+            expect(outputCmp.textContent).toBe('Some Record Name');
+            outputCmp.click();
+        });
 
-        expect(pageReference.type).toBe("standard__recordPage");
-        expect(pageReference.attributes.recordId).toBe('fake_record_id');
-        expect(pageReference.attributes.actionName).toBe("view");
+        it('navigates on click', () => {
+            const outputCmp = component.shadowRoot.querySelector('a');
 
+            outputCmp.click();
+
+            const { pageReference } = getNavigateCalledWith();
+            expect(pageReference.type).toBe("standard__recordPage");
+            expect(pageReference.attributes.recordId).toBe('fake_record_id');
+            expect(pageReference.attributes.actionName).toBe("view");
+        });
     });
-
-    it('navigates on click', () => {
-
-    });
-
 
 });
