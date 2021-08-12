837b6372818fee7f2039885c2eb42aa8a7174acf
Prince Sadie <psadie@salesforce.com>
2021-08-12 12:46:26 -0700

    fix typo

--- force-app/main/default/lwc/geGiftEntryFormApp/geGiftEntryFormApp.js
+++ force-app/main/default/lwc/geGiftEntryFormApp/geGiftEntryFormApp.js
@@ -15,7 +15,7 @@ import geBatchGiftsExpectedTotalsMessage
 import geBatchGiftsExpectedCountOrTotalMessage
     from '@salesforce/label/c.geBatchGiftsExpectedCountOrTotalMessage';
 import processBatch from '@salesforce/apex/GE_GiftEntryController.processGiftsFor';
-import checkForElevateCustomer from '@Salesforce/apex/GE_GiftEntryController.isElevateCustomer';
+import checkForElevateCustomer from '@salesforce/apex/GE_GiftEntryController.isElevateCustomer';
 
 /*******************************************************************************
 * @description Schema imports
