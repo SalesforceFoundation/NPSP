fc8c88df2875f721047837df46c69fe58414322e
Daniel Fuller <d.fuller@salesforce.com>
2021-11-30 17:03:59 -0500

    remove parent context redirect

--- force-app/main/default/aura/RD2_EntryForm/RD2_EntryFormHelper.js
+++ force-app/main/default/aura/RD2_EntryForm/RD2_EntryFormHelper.js
@@ -1,18 +1,5 @@
 ({
     /**
-     * @description: Create the modal when initializing the component
-     */
-    handleShowModal: function (component, event, helper) {
-        let recordId = component.get('v.recordId');
-        let parentId;
-
-        if (!recordId) {
-            parentId = helper.getParentId();
-            component.set('v.parentId', parentId);
-        }
-    },
-
-    /**
     * @description Handle events sent from the modal
     */
     handleModalEvent: function (component, event) {
@@ -23,9 +10,7 @@
      * @description: Redirect the page to either parent or RD record
      */
     redirectToPage: function (component, event) {
-        let navigateToId = component.get('v.parentId') || event.getParams('detail').recordId;
-
-        let navEvt = this.constructNavigationEvent(navigateToId);
+        let navEvt = this.constructNavigationEvent(event.getParams('detail').recordId);
         navEvt.fire();
     },
 
@@ -50,39 +35,5 @@
         }
 
         return navEvt;
-    },
-
-    /**
-    * @description: Extract and decode the Base64 component fragment from the URL to get the parent Id.
-    * If the target fragment is not found, return a blank string or null.
-    * @variable syntax A hardcoded text that is present in the URL when clicking 'New' button from Related List
-    * @variable regex This regex expression targets the specific base64 encoded parameter in the URL.
-    * @variable encodedParameters Extract out the target base64 encoded fragment. The regex should return 3 parts of parameters
-    *   (with syntax, with '=1.', and pure base64 encoded fragment). The third part is the pure based64 encoded fragment
-    * @variable decodedFragment Replace any '+' sign since it's replaced from space in URL
-    *   and using the standard decodeURIComponent() function to decode the component
-    * @return ParentId using wubdiw.atob() to convert the base64 string.
-    * 
-    */
-    getParentId: function () {
-        try {
-            let syntax = 'inContextOfRef';
-            syntax = syntax.replace(/[\[\]]/g, "\\$&");
-            var url = window.location.href;
-            var regex = new RegExp("[?&]" + syntax + "(=1\.([^&#]*)|&|#|$)");
-            var encodedParameters = regex.exec(url);
-
-            if (!encodedParameters) {
-                return null;
-            } else if (!encodedParameters[2]) {
-                return '';
-            }
-
-            const decodedFragment = decodeURIComponent(encodedParameters[2].replace(/\+/g, " "));
-            return JSON.parse(window.atob(decodedFragment)).attributes.recordId;
-
-        } catch (error) {
-            console.error(JSON.stringify(error));
-        }
     }
 })
