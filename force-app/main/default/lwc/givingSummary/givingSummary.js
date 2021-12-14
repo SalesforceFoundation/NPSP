3eeb8a7dee836a73ecd1e3868695a2e0247283c2
JoseRossiSF <79223486+JoseRossiSF@users.noreply.github.com>
2021-12-14 12:51:09 -0300

    using wire getrecords method as per comments

--- force-app/main/default/lwc/givingSummary/givingSummary.js
+++ force-app/main/default/lwc/givingSummary/givingSummary.js
@@ -1,11 +1,11 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 import donorsGivingSummary from '@salesforce/label/c.donorsGivingSummary';
 import donorsLifetime from '@salesforce/label/c.donorsLifetime';
 import donorsThisYear from '@salesforce/label/c.donorsThisYear';
 import flsReadAccessError from '@salesforce/label/c.flsReadAccessError';
 import donorsPreviousYear from '@salesforce/label/c.donorsPreviousYear';
 import FORM_FACTOR from '@salesforce/client/formFactor';
-import getDonationsSummary from '@salesforce/apex/GivingSummaryController.getDonationsSummaryForContact'
 
 const FormFactorType = Object.freeze({
     Large: 'Large',
@@ -15,6 +15,7 @@ const FormFactorType = Object.freeze({
 
 const MOBILE_CLASSES = 'slds-text-align_center slds-border_bottom slds-text-heading_small slds-var-p-around_medium';
 const DESKTOP_CLASSES = 'slds-text-align_center slds-border_right slds-text-heading_small';
+const FIELDS = ['Contact.npo02__TotalOppAmount__c', 'Contact.npo02__OppAmountThisYear__c', 'Contact.npo02__OppAmountLastYear__c'];
 
 export default class GivingSummary extends LightningElement {
     labels = {
@@ -29,32 +30,29 @@ export default class GivingSummary extends LightningElement {
     thisYear = 0;
     previousYear = 0;
     
-    error;
-    isAccessible=true;
+    contact;
+    @api contactId;
 
-    @api contactId
-
+    message;
+    
     formFactor = FORM_FACTOR;
 
-    connectedCallback() {
-        getDonationsSummary({contactId: this.contactId})
-            .then(contact => this.parseContact(contact))
-            .catch((error) => {
-                this.error = error;
-                this.isAccessible=false;
-            });
-        
-    }
-
-    /**
-     * get the contact from the database and populate the filds that are going to be populated.
-     * @param {*} contact theContact to parse
-     */
-    parseContact(contact) {
-        this.lifetimeSummary = contact.npo02__TotalOppAmount__c;
-        this.thisYear = contact.npo02__OppAmountThisYear__c;
-        this.previousYear = contact.npo02__OppAmountLastYear__c;
-    }
+    @wire(getRecord, { recordId: '$contactId', fields: FIELDS})
+        wiredRecord({ error, data }) {
+            if (error) {
+                    this.message = 'Unknown error';
+                if (Array.isArray(error.body)) {
+                    this.message = error.body.map(e => e.message).join(', ');
+                } else if (typeof error.body.message === 'string') {
+                    this.message = error.body.message;
+                }
+            } else if (data) {
+                this.contact = data;
+                this.lifetimeSummary = this.contact.fields.npo02__TotalOppAmount__c.value;
+                this.thisYear = this.contact.fields.npo02__OppAmountThisYear__c.value;
+                this.previousYear = this.contact.fields.npo02__OppAmountLastYear__c.value;
+            }
+        }
 
     /**
      * @description Returns the classes to be applied to the rows accordling if it is mobile or desktop
