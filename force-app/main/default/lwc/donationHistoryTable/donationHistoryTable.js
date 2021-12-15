44100ff5c4b63b1181971bb02a93b33c8ee4b2d8
mcasco <mcasco@salesforce.com>
2021-12-15 14:00:15 -0300

    implement list donation class on component, delete logs, add method to get total of records for infinite scroll, delete unnecessary mocked data

--- force-app/main/default/lwc/donationHistoryTable/donationHistoryTable.js
+++ force-app/main/default/lwc/donationHistoryTable/donationHistoryTable.js
@@ -1,17 +1,19 @@
 import { api, LightningElement, track, wire } from 'lwc';
-import {MOCK_DATA} from './donationHistoryTableData';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import DATA_IMPORT from '@salesforce/schema/DataImport__c';
 import PAYMENT_FIELD from '@salesforce/schema/DataImport__c.Payment_Method__c';
 import donationHistoryDatatableAriaLabel from '@salesforce/label/c.donationHistoryDatatableAriaLabel';
 import RD2_ScheduleVisualizerColumnDate from '@salesforce/label/c.RD2_ScheduleVisualizerColumnDate';
 import getDonationHistory from '@salesforce/apex/ListDonation.getDonationHistory';
+import getTotalRecords from '@salesforce/apex/ListDonation.getTotalRecords';
 import commonAmount from '@salesforce/label/c.commonAmount';
 import donorLabel from '@salesforce/label/c.donorLabel';
 export default class DonationHistoryTable extends LightningElement {
     @api contactId;
 
     paymentMethodLabel;
+
+    totalNumberOfRows;
 
     data = [];
 
@@ -38,21 +40,21 @@ export default class DonationHistoryTable extends LightningElement {
             { label: this.paymentMethodLabel, fieldName: 'paymentMethod', type: 'text', },
         ];
     }
-
-    @wire(getDonationHistory, { contactId: '$contactId' })
-    getDonation({ data, error }) {
-        if (data) {
-            console.log(JSON.stringify(data));
-            this.data = data;
-        }
-    }
-
-    
-    
-    totalNumberOfRows = MOCK_DATA.length;
     
     // eslint-disable-next-line @lwc/lwc/no-async-await
     async connectedCallback() {
+        getTotalRecords({contactId: this.contactId})
+        .then({
+            if(data){
+                this.totalNumberOfRows = data[0].expr0;
+            }
+        });
+        getDonationHistory({contactId: this.contactId, offset: 0})
+        .then(data => {
+            if (data) {
+                this.data = data;
+            }
+        });
     }
 
     /**
@@ -61,18 +63,15 @@ export default class DonationHistoryTable extends LightningElement {
      * handle the scroll event to get more content and concat to the existing table data
      */
     loadMoreDonationData(event){
-        //TODO: add wired service
-        //Display a spinner to signal that data is being loaded
-        event.target.isLoading = true;
         if (this.data.length >= this.totalNumberOfRows) {
             event.target.enableInfiniteLoading = false;
-            event.target.isLoading = false;
             return;
         }
-        const currentData = this.data;
-        //Appends new data to the end of the table
-        const newData = currentData.concat(MOCK_DATA.slice(50, 70));
-        this.data = newData;
-        event.target.isLoading = false;
+        getDonationHistory({contactId: this.contactId, offset: this.data.length})
+        .then(data => {
+            const currentData = this.data;
+            const newData = currentData.concat(data);
+            this.data = newData;
+        });
     }
 }
