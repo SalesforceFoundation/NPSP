264b2ea3204de8e8948132b4e2f3c70508fd917d
Benjamin Dvorachek <bdvorachek@salesforce.com>
2022-01-14 17:34:34 -0600

    rd2EntryFormDonorSection.test.js - reformat / add test for dateEstablished events

--- force-app/main/default/lwc/rd2EntryFormDonorSection/__tests__/rd2EntryFormDonorSection.test.js
+++ force-app/main/default/lwc/rd2EntryFormDonorSection/__tests__/rd2EntryFormDonorSection.test.js
@@ -1,37 +1,38 @@
 import { createElement } from "lwc";
-import Rd2EntryFormDonorSection from 'c/rd2EntryFormDonorSection';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import Rd2EntryFormDonorSection from "c/rd2EntryFormDonorSection";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
 
-import getRecurringData from '@salesforce/apex/RD2_EntryFormController.getRecurringData';
-import RECURRING_DONATION_OBJECT from '@salesforce/schema/npe03__Recurring_Donation__c';
+import getRecurringData from "@salesforce/apex/RD2_EntryFormController.getRecurringData";
+import RECURRING_DONATION_OBJECT from "@salesforce/schema/npe03__Recurring_Donation__c";
 
 
-const recurringDataAccountResponse = require('../../../../../../tests/__mocks__/apex/data/recurringDataAccountResponse.json');
-const recurringDataContactResponse = require('../../../../../../tests/__mocks__/apex/data/recurringDataContactResponse.json');
-const recurringDonationObjectInfo = require('../../../../../../tests/__mocks__/apex/data/recurringDonationObjectInfo.json');
+const recurringDataAccountResponse = require("../../../../../../tests/__mocks__/apex/data/recurringDataAccountResponse.json");
+const recurringDataContactResponse = require("../../../../../../tests/__mocks__/apex/data/recurringDataContactResponse.json");
+const recurringDonationObjectInfo = require("../../../../../../tests/__mocks__/apex/data/recurringDonationObjectInfo.json");
 
 const mockHandleContactChange = jest.fn();
 const mockHandleAccountChange = jest.fn();
 const mockHandleDonorTypeChange = jest.fn();
+const mockHandleDateEstablishedChange = jest.fn();
 
-const FAKE_RD_ID = 'fake-rd-record-id';
-const FAKE_ACCOUNT_ID = 'fake-account-record-id';
-const FAKE_CONTACT_ID = 'fake-contact-record-id';
+const FAKE_RD_ID = "fake-rd-record-id";
+const FAKE_ACCOUNT_ID = "fake-account-record-id";
+const FAKE_CONTACT_ID = "fake-contact-record-id";
 
 const assertDonorTypeFieldValue = (element, expectedValue) => {
     const donorTypeField = getDonorTypeField(element);
     expect(donorTypeField).toBeTruthy();
-    expect(donorTypeField.options).toContainOptions(['Contact', 'Account']);
+    expect(donorTypeField.options).toContainOptions(["Contact", "Account"]);
     expect(donorTypeField.value).toBe(expectedValue);
-}
+};
 
-describe('new recurring donation', () => {
+describe("new recurring donation", () => {
     let element;
 
     beforeEach(async () => {
         element = createDonorSection();
-        getObjectInfo.emit(recurringDonationObjectInfo, ({objectApiName}) => {
-            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName
+        getObjectInfo.emit(recurringDonationObjectInfo, ({ objectApiName }) => {
+            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName;
         });
         await flushPromises();
     });
@@ -41,19 +42,19 @@ describe('new recurring donation', () => {
         clearDOM();
     });
 
-    it('donor type picklist defaults to Contact', () => {
-        assertDonorTypeFieldValue(element, 'Contact');
+    it("donor type picklist defaults to Contact", () => {
+        assertDonorTypeFieldValue(element, "Contact");
     });
 
 
-    it('when set to Account, then account lookup appears', async () => {
+    it("when set to Account, then account lookup appears", async () => {
 
         const donorTypeField = getDonorTypeField(element);
-        donorTypeField.value = 'Account';
-        donorTypeField.dispatchEvent(new CustomEvent('change', { detail: { value: 'Account' } }));
+        donorTypeField.value = "Account";
+        donorTypeField.dispatchEvent(new CustomEvent("change", { detail: { value: "Account" } }));
         await flushPromises();
         const lastEventDetail = getLastEventDetail(mockHandleDonorTypeChange);
-        expect(lastEventDetail).toBe('Account');
+        expect(lastEventDetail).toBe("Account");
 
         const accountLookup = getAccountLookup(element);
         expect(accountLookup).toBeTruthy();
@@ -61,14 +62,14 @@ describe('new recurring donation', () => {
 
 });
 
-describe('existing recurring contact donation', () => {
+describe("existing recurring contact donation", () => {
     let element;
 
     beforeEach(async () => {
         getRecurringData.mockResolvedValue(recurringDataContactResponse);
         element = createDonorSection({ recordId: FAKE_RD_ID });
-        getObjectInfo.emit(recurringDonationObjectInfo, ({objectApiName}) => {
-            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName
+        getObjectInfo.emit(recurringDonationObjectInfo, ({ objectApiName }) => {
+            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName;
         });
         await flushPromises();
     });
@@ -76,21 +77,21 @@ describe('existing recurring contact donation', () => {
     afterEach(() => {
         clearDOM();
         jest.clearAllMocks();
-    })
-
-    it('loads with donor type set to contact', () => {
-        assertDonorTypeFieldValue(element, 'Contact');
     });
 
-    it('notifies parent component of donor type change', () => {
+    it("loads with donor type set to contact", () => {
+        assertDonorTypeFieldValue(element, "Contact");
+    });
+
+    it("notifies parent component of donor type change", () => {
         expect(mockHandleDonorTypeChange).toHaveBeenCalled();
         const lastEventDetail = getLastEventDetail(mockHandleDonorTypeChange);
-        expect(lastEventDetail).toBe('Contact');
+        expect(lastEventDetail).toBe("Contact");
     });
 });
 
 
-describe('existing recurring account donation', () => {
+describe("existing recurring account donation", () => {
     let element;
 
     beforeEach(async () => {
@@ -98,8 +99,8 @@ describe('existing recurring account donation', () => {
 
         element = createDonorSection({ recordId: FAKE_RD_ID });
 
-        getObjectInfo.emit(recurringDonationObjectInfo, ({objectApiName}) => {
-            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName
+        getObjectInfo.emit(recurringDonationObjectInfo, ({ objectApiName }) => {
+            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName;
         });
 
         await flushPromises();
@@ -110,20 +111,20 @@ describe('existing recurring account donation', () => {
         jest.clearAllMocks();
     });
 
-    it('loads with donor type set to Account', () => {
+    it("loads with donor type set to Account", () => {
         expect(getRecurringData).toHaveBeenCalled();
-        assertDonorTypeFieldValue(element, 'Account');
+        assertDonorTypeFieldValue(element, "Account");
     });
 
-    it('clears values in Account lookup when donor type changed to Contact', async () => {
-        changeDonorType(element, 'Contact');
-        assertDonorTypeFieldValue(element, 'Contact');
+    it("clears values in Account lookup when donor type changed to Contact", async () => {
+        changeDonorType(element, "Contact");
+        assertDonorTypeFieldValue(element, "Contact");
 
         await flushPromises();
 
         expect(getAccountLookup(element)).toBeNull();
 
-        changeDonorType(element, 'Account');
+        changeDonorType(element, "Account");
         await flushPromises();
 
         const accountLookup = getAccountLookup(element);
@@ -132,16 +133,16 @@ describe('existing recurring account donation', () => {
     });
 });
 
-describe('new donation from account', () => {
+describe("new donation from account", () => {
     let element;
 
     beforeEach(async () => {
         getRecurringData.mockResolvedValue(recurringDataAccountResponse);
 
-        element = createDonorSection({ parentId: FAKE_ACCOUNT_ID, parentSObjectType: 'Account' });
+        element = createDonorSection({ parentId: FAKE_ACCOUNT_ID, parentSObjectType: "Account" });
 
-        getObjectInfo.emit(recurringDonationObjectInfo, ({objectApiName}) => {
-            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName
+        getObjectInfo.emit(recurringDonationObjectInfo, ({ objectApiName }) => {
+            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName;
         });
 
         await flushPromises();
@@ -152,19 +153,19 @@ describe('new donation from account', () => {
         jest.clearAllMocks();
     });
 
-    it('alerts parent component of accountId', () => {
+    it("alerts parent component of accountId", () => {
         const lastEventDetail = getLastEventDetail(mockHandleAccountChange);
         expect(lastEventDetail).toBe(FAKE_ACCOUNT_ID);
     });
 
-    it('populates account lookup', () => {
+    it("populates account lookup", () => {
         const accountLookup = getAccountLookup(element);
         expect(accountLookup).toBeTruthy();
         expect(accountLookup.value).toBe(FAKE_ACCOUNT_ID);
     });
 
-    it('clears account lookup on donor type change', async () => {
-        changeDonorType(element, 'Contact');
+    it("clears account lookup on donor type change", async () => {
+        changeDonorType(element, "Contact");
         await flushPromises();
         const accountLookup = getAccountLookup(element);
         expect(accountLookup).toBeNull();
@@ -175,16 +176,16 @@ describe('new donation from account', () => {
 });
 
 
-describe('new donation from contact', () => {
+describe("new donation from contact", () => {
     let element;
 
     beforeEach(async () => {
         getRecurringData.mockResolvedValue(recurringDataAccountResponse);
 
-        element = createDonorSection({ parentId: FAKE_CONTACT_ID, parentSObjectType: 'Contact' });
+        element = createDonorSection({ parentId: FAKE_CONTACT_ID, parentSObjectType: "Contact" });
 
-        getObjectInfo.emit(recurringDonationObjectInfo, ({objectApiName}) => {
-            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName
+        getObjectInfo.emit(recurringDonationObjectInfo, ({ objectApiName }) => {
+            return objectApiName === RECURRING_DONATION_OBJECT.objectApiName;
         });
 
         await flushPromises();
@@ -195,19 +196,29 @@ describe('new donation from contact', () => {
         clearDOM();
     });
 
-    it('alerts parent component of contactId', () => {
-       const lastEventDetail = getLastEventDetail(mockHandleContactChange);
-       expect(lastEventDetail).toBe(FAKE_CONTACT_ID);
+    it("alerts parent component of contactId", () => {
+        const lastEventDetail = getLastEventDetail(mockHandleContactChange);
+        expect(lastEventDetail).toBe(FAKE_CONTACT_ID);
     });
 
-    it('populates contact lookup', () => {
+    it("alerts parent component of date established changes", () => {
+        const dateEstablished = getDateEstablishedField(element);
+        expect(dateEstablished).toBeTruthy();
+        const fakeDate = new Date(2022, 0, 14)
+        changeField(dateEstablished, fakeDate);
+        const lastEventDetail = getLastEventDetail(mockHandleDateEstablishedChange);
+
+        expect(lastEventDetail).toBe(fakeDate);
+    });
+
+    it("populates contact lookup", () => {
         const contactLookup = getContactLookup(element);
         expect(contactLookup).toBeTruthy();
         expect(contactLookup.value).toBe(FAKE_CONTACT_ID);
     });
 
-    it('when donor type changed to account, contact lookup cleared', async () => {
-        changeDonorType(element, 'Account');
+    it("when donor type changed to account, contact lookup cleared", async () => {
+        changeDonorType(element, "Account");
         await flushPromises();
         const contactLookup = getContactLookup(element);
 
@@ -215,44 +226,53 @@ describe('new donation from contact', () => {
         const accountLookup = getAccountLookup(element);
         expect(accountLookup).toBeTruthy();
     });
-
 });
 
 const getLastEventDetail = (mockedFn) => {
     const { calls } = mockedFn.mock;
     const lastIndex = calls.length - 1;
     return calls[lastIndex][0].detail;
-}
+};
 
 const changeDonorType = (element, value) => {
     const field = getDonorTypeField(element);
-    field.value = value;
-    field.dispatchEvent(new CustomEvent('change', { detail: { value } }));
+    changeField(field, value);
 }
+
+const changeField = (field, value) => {
+    field.value = value;
+    field.dispatchEvent(new CustomEvent("change", { detail: { value } }));
+};
 
 const getDonorTypeField = (element) => {
-    return element.shadowRoot.querySelector('lightning-combobox[data-id="donorType"]');
-}
+    return element.shadowRoot.querySelector("lightning-combobox[data-id=\"donorType\"]");
+};
 
 const getAccountLookup = (element) => {
-    return element.shadowRoot.querySelector('lightning-input-field[data-id="accountLookup"]');
-}
+    return element.shadowRoot.querySelector("lightning-input-field[data-id=\"accountLookup\"]");
+};
 
 const getContactLookup = (element) => {
-    return element.shadowRoot.querySelector('lightning-input-field[data-id="contactLookup"]');
+    return element.shadowRoot.querySelector("lightning-input-field[data-id=\"contactLookup\"]");
+};
+
+const getDateEstablishedField = (element) => {
+    return element.shadowRoot.querySelector("lightning-input-field[data-id=\"dateEstablished\"]");
 }
 
 const createDonorSection = (params) => {
-    const donorSection = createElement('c-rd2-entry-form-donor-section', { is: Rd2EntryFormDonorSection });
-    if(params) {
+    const donorSection = createElement("c-rd2-entry-form-donor-section", { is: Rd2EntryFormDonorSection });
+    if (params) {
         const { recordId, parentId, parentSObjectType } = params;
         donorSection.recordId = recordId;
         donorSection.parentId = parentId;
         donorSection.parentSObjectType = parentSObjectType;
     }
-    donorSection.addEventListener('donortypechange', mockHandleDonorTypeChange);
-    donorSection.addEventListener('accountchange', mockHandleAccountChange);
-    donorSection.addEventListener('contactchange', mockHandleContactChange);
+    donorSection.addEventListener("donortypechange", mockHandleDonorTypeChange);
+    donorSection.addEventListener("accountchange", mockHandleAccountChange);
+    donorSection.addEventListener("contactchange", mockHandleContactChange);
+    donorSection.addEventListener("dateestablishedchange", mockHandleDateEstablishedChange);
+
     document.body.appendChild(donorSection);
     return donorSection;
 };
