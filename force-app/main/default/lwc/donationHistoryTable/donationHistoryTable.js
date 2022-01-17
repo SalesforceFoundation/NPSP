aaac81b66605c6cba4e6cb61332797b28a3176cf
mcasco <mcasco@salesforce.com>
2022-01-17 17:22:28 -0300

    implement infinite scroll for donation history component

--- force-app/main/default/lwc/donationHistoryTable/donationHistoryTable.js
+++ force-app/main/default/lwc/donationHistoryTable/donationHistoryTable.js
@@ -5,13 +5,13 @@ import PAYMENT_FIELD from '@salesforce/schema/DataImport__c.Payment_Method__c';
 import donationHistoryDatatableAriaLabel from '@salesforce/label/c.donationHistoryDatatableAriaLabel';
 import RD2_ScheduleVisualizerColumnDate from '@salesforce/label/c.RD2_ScheduleVisualizerColumnDate';
 import getDonationHistory from '@salesforce/apex/DonationHistoryController.getDonationHistory';
+import getTotalOfRows from '@salesforce/apex/DonationHistoryController.getTotalOfRows';
 import commonAmount from '@salesforce/label/c.commonAmount';
 import donationHistoryDonorLabel from '@salesforce/label/c.donationHistoryDonorLabel';
 const RECORDS_TO_LOAD = 50;
 export default class DonationHistoryTable extends LightningElement {
     @api contactId;
 
-    
     paymentMethodLabel;
 
     totalNumberOfRows;
@@ -48,12 +48,17 @@ export default class DonationHistoryTable extends LightningElement {
     
     // eslint-disable-next-line @lwc/lwc/no-async-await
     async connectedCallback() {
-        getDonationHistory({contactId: this.contactId})
+        getTotalOfRows({contactId: this.contactId})
+        .then(data => {
+            if(data){
+                this.totalNumberOfRows = data[0].expr0;
+            }
+        });
+        getDonationHistory({contactId: this.contactId, offset: 0})
         .then(data => {
             if (data) {
                 this.allData = data;
                 this.data = data.slice(0, RECORDS_TO_LOAD);
-                this.totalNumberOfRows = data.length;
             }
         });
     }
@@ -65,17 +70,19 @@ export default class DonationHistoryTable extends LightningElement {
      */
     loadMoreDonationData(event){
         event.target.isLoading = true;
-        if (this.data.length >= this.totalNumberOfRows) {
-            event.target.enableInfiniteLoading = false;
-            event.target.isLoading = false;
-            return;
-        }
-        const current = this.data.length;
-        const offset = current + RECORDS_TO_LOAD;
-        const currentData = this.data;
-        //Appends new data to the end of the table
-        const newData = currentData.concat(this.allData.slice(current, offset));
-        this.data = newData;
-        event.target.isLoading = false;
+        this.tableElement = event.target;
+        getDonationHistory({contactId: this.contactId, offset: this.data.length})
+        .then(data => {
+                if (this.data.length >= this.totalNumberOfRows) {
+                    this.tableElement.isLoading = false;
+                    this.tableElement.enableInfiniteLoading = false;
+                    return;
+                }
+                const currentData = this.data;
+                const newData = currentData.concat(data);
+                this.data = newData;
+                //this fails for reference
+                this.tableElement.isLoading = false;
+        });
     }
 }
