780fe95db95eeb1a617371747469c9b16bc6db26
JoseRossiSF <79223486+JoseRossiSF@users.noreply.github.com>
2022-01-19 11:06:17 -0300

    adding further changes required by Diego

--- force-app/main/default/lwc/donationHistoryTable/donationHistoryTable.js
+++ force-app/main/default/lwc/donationHistoryTable/donationHistoryTable.js
@@ -1,94 +1,99 @@
-import { api, LightningElement, track, wire } from 'lwc';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import DATA_IMPORT from '@salesforce/schema/DataImport__c';
-import PAYMENT_FIELD from '@salesforce/schema/DataImport__c.Payment_Method__c';
-import donationHistoryDatatableAriaLabel from '@salesforce/label/c.donationHistoryDatatableAriaLabel';
-import RD2_ScheduleVisualizerColumnDate from '@salesforce/label/c.RD2_ScheduleVisualizerColumnDate';
-import getDonationHistory from '@salesforce/apex/DonationHistoryController.getDonationHistory';
-import arePaymentsEnabled from '@salesforce/apex/DonationHistoryController.arePaymentsEnabled';
-import commonAmount from '@salesforce/label/c.commonAmount';
-import donationHistoryDonorLabel from '@salesforce/label/c.donationHistoryDonorLabel';
+import { api, LightningElement, track, wire } from "lwc";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
+import DATA_IMPORT from "@salesforce/schema/DataImport__c";
+import PAYMENT_FIELD from "@salesforce/schema/DataImport__c.Payment_Method__c";
+import donationHistoryDatatableAriaLabel from "@salesforce/label/c.donationHistoryDatatableAriaLabel";
+import RD2_ScheduleVisualizerColumnDate from "@salesforce/label/c.RD2_ScheduleVisualizerColumnDate";
+import getDonationHistory from "@salesforce/apex/DonationHistoryController.getDonationHistory";
+import arePaymentsEnabled from "@salesforce/apex/DonationHistoryController.arePaymentsEnabled";
+import commonAmount from "@salesforce/label/c.commonAmount";
+import donationHistoryDonorLabel from "@salesforce/label/c.donationHistoryDonorLabel";
 const RECORDS_TO_LOAD = 50;
 export default class DonationHistoryTable extends LightningElement {
-    @api contactId;
+  @api contactId;
 
-    paymentMethodLabel;
+  paymentMethodLabel;
 
-    totalNumberOfRows;
+  totalNumberOfRows;
 
-    tableElement;
+  tableElement;
 
-    allData = [];
+  allData = [];
 
-    data = [];
+  data = [];
 
-    label = {
-        donationHistoryDatatableAriaLabel,
+  label = {
+    donationHistoryDatatableAriaLabel,
+  };
+
+  @track
+  columns = [];
+
+  @wire(getObjectInfo, { objectApiName: DATA_IMPORT })
+  oppInfo({ data, error }) {
+    if (data) {
+      this.paymentMethodLabel = data.fields[PAYMENT_FIELD.fieldApiName].label;
     }
-
-    columns = [];
-
-    @wire(getObjectInfo, { objectApiName: DATA_IMPORT })
-    oppInfo({ data, error }) {
-        if (data) {
-            this.paymentMethodLabel = data.fields[PAYMENT_FIELD.fieldApiName].label
-        };
-        arePaymentsEnabled({contactId: this.contactId}).then( (paymentsAreEnabled) => {
-            if(paymentsAreEnabled){
-                this.columns = [
-                { label: RD2_ScheduleVisualizerColumnDate, fieldName: 'closeDate', type: 'date-local', typeAttributes:{
-                    year: "numeric",
-                    month: "numeric",
-                    day: "numeric",
-                },
-                cellAttributes: { alignment: 'right' }},
-                { label: donationHistoryDonorLabel, fieldName: 'name', type: 'text' },
-                { label: commonAmount, fieldName: 'amount', type: 'currency', },
-                { label: this.paymentMethodLabel, fieldName: 'paymentMethod', type: 'text', },];
-            }else{
-                this.columns = [
-                    { label: RD2_ScheduleVisualizerColumnDate, fieldName: 'closeDate', type: 'date-local', typeAttributes:{
-                        year: "numeric",
-                        month: "numeric",
-                        day: "numeric",
-                    },
-                    cellAttributes: { alignment: 'right' }},
-                    { label: donationHistoryDonorLabel, fieldName: 'name', type: 'text' },
-                    { label: commonAmount, fieldName: 'amount', type: 'currency', },];
-            }
-        }  );
-    }
-    
-    // eslint-disable-next-line @lwc/lwc/no-async-await
-    async connectedCallback() {
-        getDonationHistory({contactId: this.contactId})
-        .then(data => {
-            if (data) {
-                this.allData = data;
-                this.data = data.slice(0, RECORDS_TO_LOAD);
-                this.totalNumberOfRows = data.length;
-            }
-        });
-    }
-
-    /**
-     * 
-     * @param {*} event 
-     * handle the scroll event to get more content and concat to the existing table data
-     */
-    loadMoreDonationData(event){
-        event.target.isLoading = true;
-        if (this.data.length >= this.totalNumberOfRows) {
-            event.target.enableInfiniteLoading = false;
-            event.target.isLoading = false;
-            return;
+    arePaymentsEnabled( { contactId: this.contactId } ).then(
+      (paymentsAreEnabled) => {
+        console.log(paymentsAreEnabled);
+        this.columns = [
+            {
+              label: RD2_ScheduleVisualizerColumnDate,
+              fieldName: "closeDate",
+              type: "date-local",
+              typeAttributes: {
+                year: "numeric",
+                month: "numeric",
+                day: "numeric",
+              },
+              cellAttributes: { alignment: "right" },
+            },
+            {
+              label: donationHistoryDonorLabel,
+              fieldName: "name",
+              type: "text",
+            },
+            { label: commonAmount, fieldName: "amount", type: "currency" },
+        ];
+        if (paymentsAreEnabled) {
+            this.columns.push(
+                { label: this.paymentMethodLabel, fieldName: "paymentMethod", type: "text", },
+            );
         }
-        const current = this.data.length;
-        const offset = current + RECORDS_TO_LOAD;
-        const currentData = this.data;
-        //Appends new data to the end of the table
-        const newData = currentData.concat(this.allData.slice(current, offset));
-        this.data = newData;
-        event.target.isLoading = false;
+      }
+    );
+  }
+
+  // eslint-disable-next-line @lwc/lwc/no-async-await
+  async connectedCallback() {
+    getDonationHistory({ contactId: this.contactId }).then((data) => {
+      if (data) {
+        this.allData = data;
+        this.data = data.slice(0, RECORDS_TO_LOAD);
+        this.totalNumberOfRows = data.length;
+      }
+    });
+  }
+
+  /**
+   *
+   * @param {*} event
+   * handle the scroll event to get more content and concat to the existing table data
+   */
+  loadMoreDonationData(event) {
+    event.target.isLoading = true;
+    if (this.data.length >= this.totalNumberOfRows) {
+      event.target.enableInfiniteLoading = false;
+      event.target.isLoading = false;
+      return;
     }
+    const current = this.data.length;
+    const offset = current + RECORDS_TO_LOAD;
+    const currentData = this.data;
+    //Appends new data to the end of the table
+    const newData = currentData.concat(this.allData.slice(current, offset));
+    this.data = newData;
+    event.target.isLoading = false;
+  }
 }
