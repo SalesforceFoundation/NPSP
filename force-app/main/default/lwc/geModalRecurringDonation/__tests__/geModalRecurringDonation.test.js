bd06b48bb2eb7aaf09fd0f57c7f7f395123fff1b
Prince Sadie <psadie@salesforce.com>
2022-01-26 22:18:45 -0800

    add jest test to assert render behavior

--- force-app/main/default/lwc/geModalRecurringDonation/__tests__/geModalRecurringDonation.test.js
+++ force-app/main/default/lwc/geModalRecurringDonation/__tests__/geModalRecurringDonation.test.js
@@ -6,13 +6,40 @@ describe('c-ge-modal-recurring-donation', () => {
         clearDOM();
     });
 
+    const setup = (cancelCallback=jest.fn(), createRecurrenceCallback=jest.fn()) => {
+        const modalElement = createElement('c-ge-modal-recurring-donation', {
+            is: GeModalRecurringDonation
+        });
+        modalElement.cancelCallback = cancelCallback;
+        modalElement.createRecurrenceCallback = createRecurrenceCallback;
+        document.body.appendChild(modalElement);
+        return modalElement;
+    }
+
     describe('render behavior', () => {
         it('renders rd2EntryFormScheduleSection component', async () => {
+            const modalElement = setup();
+            await flushPromises();
 
+            const scheduleComponent = modalElement.shadowRoot.querySelector('[data-id="scheduleComponent"]');
+
+            expect(scheduleComponent).toBeDefined();
         });
 
-        it('renders provided cancel button', async () => {
+        it('renders cancel and create recurrence buttons', async () => {
+            const modalElement = setup();
+            await flushPromises();
 
+            const buttons = modalElement.shadowRoot.querySelectorAll('lightning-button');
+            const cancelButton = buttons[0];
+            const createRecurrenceButton = buttons[1];
+            console.log(buttons);
+
+            expect(buttons.length).toBe(2);
+            expect(cancelButton).toBeDefined();
+            expect(cancelButton.label).toBe('Cancel');
+            expect(createRecurrenceButton).toBeDefined();
+            expect(createRecurrenceButton.label).toBe('Create Recurrence');
         });
     });
 
