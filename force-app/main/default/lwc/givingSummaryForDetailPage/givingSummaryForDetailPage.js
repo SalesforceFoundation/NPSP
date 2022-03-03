8dcfc67af589af7875bae72f94e93cb29d58ad82
JoseRossiSF <79223486+JoseRossiSF@users.noreply.github.com>
2022-03-03 13:47:15 -0300

    renaming and fixing Permission Set. Adding logic to Giving Summary LWC so that it works on communities

--- force-app/main/default/lwc/givingSummaryForDetailPage/givingSummaryForDetailPage.js
+++ force-app/main/default/lwc/givingSummaryForDetailPage/givingSummaryForDetailPage.js
@@ -1,7 +1,18 @@
 import { api, LightningElement } from 'lwc';
+import getContactIdByUserId from "@salesforce/apex/DonationHistoryController.getContactIdByUserId";
 
 export default class GivingSummaryDetailPage extends LightningElement {
     
     @api recordId;
+
+    // eslint-disable-next-line @lwc/lwc/no-async-await
+    async connectedCallback() {
+        if(!this.recordId){
+            getContactIdByUserId()
+            .then((recordId) => {
+                this.recordId = recordId;
+            });
+        } 
+    }
 
 }
