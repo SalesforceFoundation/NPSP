bc2b257b887ea1a852f9f0cb3dc6534adb1439f7
JoseRossiSF <79223486+JoseRossiSF@users.noreply.github.com>
2022-03-08 11:40:38 -0300

    adding logic to solve merge conflicts with refactor

--- force-app/main/default/lwc/donationHistoryTable/donationHistoryTable.js
+++ force-app/main/default/lwc/donationHistoryTable/donationHistoryTable.js
@@ -1,14 +1,22 @@
-import { api, LightningElement, track } from "lwc";
-import donationHistoryDatatableAriaLabel from "@salesforce/label/c.donationHistoryDatatableAriaLabel";
+import { api, LightningElement, wire, track } from 'lwc';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import DATA_IMPORT from '@salesforce/schema/DataImport__c';
+import PAYMENT_FIELD from '@salesforce/schema/DataImport__c.Payment_Method__c';
+import donationHistoryDatatableAriaLabel from '@salesforce/label/c.donationHistoryDatatableAriaLabel';
+import RD2_ScheduleVisualizerColumnDate from '@salesforce/label/c.RD2_ScheduleVisualizerColumnDate';
+import getDonationHistory from '@salesforce/apex/DonationHistoryController.getDonationHistory';
+import commonAmount from '@salesforce/label/c.commonAmount';
+import donationHistoryDonorLabel from '@salesforce/label/c.donationHistoryDonorLabel';
 import commonDate from "@salesforce/label/c.commonDate";
-import getDonationHistory from "@salesforce/apex/DonationHistoryController.getDonationHistory";
-import commonAmount from "@salesforce/label/c.commonAmount";
-import donationHistoryDonorLabel from "@salesforce/label/c.donationHistoryDonorLabel";
 import getContactIdByUserId from "@salesforce/apex/DonationHistoryController.getContactIdByUserId";
 
 const RECORDS_TO_LOAD = 50;
 export default class DonationHistoryTable extends LightningElement {
     @api contactId;
+
+    _filter;
+
+    infiniteScroll = true;
 
     paymentMethodLabel;
 
@@ -34,18 +42,53 @@ export default class DonationHistoryTable extends LightningElement {
             getContactIdByUserId()
             .then((contactId) => {
                 this.contactId = contactId;
-                this.getDonations(contactId);
+                this.retrieveDonationHistory();
             });
         } else {
-            this.getDonations(this.contactId);
+            this.retrieveDonationHistory();
         }
     }
 
-    getDonations(contactId){
-        getDonationHistory({ contactId: this.contactId, offset: 0 }).then((data) => {
+
+    @wire(getObjectInfo, { objectApiName: DATA_IMPORT })
+    oppInfo({ data, error }) {
+        if (data) {
+            this.paymentMethodLabel = data.fields[PAYMENT_FIELD.fieldApiName].label
+        };
+        this.columns = [
+            { label: RD2_ScheduleVisualizerColumnDate, fieldName: 'closeDate', type: 'date-local', typeAttributes:{
+                year: "numeric",
+                month: "numeric",
+                day: "numeric",
+            },
+            cellAttributes: { alignment: 'right' }},
+            { label: donationHistoryDonorLabel, fieldName: 'name', type: 'text' },
+            { label: commonAmount, fieldName: 'amount', type: 'currency', },
+            { label: this.paymentMethodLabel, fieldName: 'paymentMethod', type: 'text', },
+        ];
+    }
+
+    @api
+    get filter() {
+        return this._filter;
+    }
+
+    set filter(value) {
+        this.data = [];
+        this._filter = value;
+        this.retrieveDonationHistory();
+        this.infiniteScroll = true;
+        this.tableElement ? this.tableElement.enableInfiniteLoading = true : null;
+    }
+
+    // eslint-disable-next-line @lwc/lwc/no-async-await
+    async retrieveDonationHistory(){
+        getDonationHistory({ contactId: this.contactId, offset: 0, filter : this.filter })
+        .then((data) => {
 
             if (data) {
                 this.totalNumberOfRecords = data.totalNumberOfRecords;
+                console.log('this.totalNumberOfRecords: ',this.totalNumberOfRecords);
                 this.data = data.donations;
                 this.paymentMethodLabel = data.paymentMethodLabel;
                 this.arePaymentsEnabled = data.isPaymentsEnabled;
@@ -85,13 +128,15 @@ export default class DonationHistoryTable extends LightningElement {
      * @param {*} event
      * handle the scroll event to get more content and concat to the existing table data
      */
-    loadMoreDonationData(event) {
+     loadMoreDonationData(event) {
         event.target.isLoading = true;
         this.tableElement = event.target;
-        getDonationHistory({contactId: this.contactId, offset: this.data.length})
+        getDonationHistory({contactId: this.contactId, offset: this.data.length, filter: this.filter})
         .then(data => {
                 if (this.data.length >= this.totalNumberOfRecords) {
+                    console.log('this.data.length: ', this.data.length);
                     this.tableElement.isLoading = false;
+                    this.infiniteScroll = false;
                     this.tableElement.enableInfiniteLoading = false;
                     return;
                 }
