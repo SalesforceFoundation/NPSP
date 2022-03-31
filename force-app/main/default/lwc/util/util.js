e3082a2b24bd7ad8114016caf3e2ed0837ba4902
Benjamin Dvorachek <bdvorachek@salesforce.com>
2022-03-31 12:22:46 -0500

    util.js - Reformat w/ Prettier.

--- force-app/main/default/lwc/util/util.js
+++ force-app/main/default/lwc/util/util.js
@@ -1,13 +1,14 @@
-
 /**
- * Returns if value equals undefinied or null
+ * Returns TRUE when value is undefined or null
  * @param {*} value - Anything
  */
-const isNull = (value) => { return value === undefined || value === null };
+const isNull = (value) => {
+    return value === undefined || value === null;
+};
 
 /**
  * Returns if isNull(value) is true or if value is a string and is empty or all whitespace
- * @param {*} value 
+ * @param {*} value
  */
 const isBlank = (value) => {
     return isNull(value) || (typeof value === "string" && value.match(/^\s*$/));
@@ -15,15 +16,10 @@ const isBlank = (value) => {
 
 const isEmpty = (obj) => {
     return Object.keys(obj).length === 0;
-}
+};
 
 const nullIfBlank = (value) => {
     return isBlank(value) ? null : value;
-}
-
-export {
-    isNull,
-    isBlank,
-    isEmpty,
-    nullIfBlank
 };
+
+export { isNull, isBlank, isEmpty, nullIfBlank };
