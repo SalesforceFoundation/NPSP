54f5ee0bac7bcb68b57d6bd02dc0659376bf3c0f
mcasco <mcasco@salesforce.com>
2022-05-26 01:09:40 -0400

    add focus trapping to ensure focus only on active modal

--- force-app/main/default/lwc/stopRecurringDonationModal/stopRecurringDonationModal.js
+++ force-app/main/default/lwc/stopRecurringDonationModal/stopRecurringDonationModal.js
@@ -1,6 +1,12 @@
 import { LightningElement, api } from 'lwc';
 import stopRecurringDonation from '@salesforce/label/c.stopRecurringDonation';
 import stopRecurringDonationModalTitle from '@salesforce/label/c.stopRecurringDonationModalTitle';
+
+const ESC_KEY_CODE = 27;
+const ESC_KEY_STRING = "Escape";
+const FOCUSABLE_ELEMENTS = "button";
+const TAB_KEY_CODE = 9;
+const TAB_KEY_STRING = "Tab";
 export default class StopRecurringDonationModal extends LightningElement {
 
     labels = {
@@ -10,7 +16,39 @@ export default class StopRecurringDonationModal extends LightningElement {
 
     @api openStopRecurringDonation;
 
-    closeModal() {
+    renderedCallback() {
+        this.template.addEventListener("keydown", (e) => this.handleKeyUp(e));
+      }
+  
+      handleKeyUp(e) {
+          const firstFocusableElement = this._getFocusableElements()[0];
+          const focusableContent = this._getFocusableElements();
+          const lastFocusableElement = focusableContent[focusableContent.length - 1];
+        
+          if (e.shiftKey) {
+            if (this.template.activeElement === firstFocusableElement) {
+              lastFocusableElement.focus();
+              e.preventDefault();
+            }
+          } else if(e.code === ESC_KEY_STRING || e.keyCode === ESC_KEY_CODE) {
+            this.closeModal();
+          } else if(e.code === TAB_KEY_STRING || e.keyCode === TAB_KEY_CODE) {
+            if (this.template.activeElement === lastFocusableElement) {
+              firstFocusableElement.focus();
+              e.preventDefault();
+            }
+          }
+      }   
+  
+      _getFocusableElements() {
+        const potentialElems = [
+          ...this.template.querySelectorAll(FOCUSABLE_ELEMENTS),
+        ];
+        return potentialElems;
+      }
+  
+      closeModal() {
+        this.template.removeEventListener("keydown", (e) => this.handleKeyUp(e));
         this.dispatchEvent(new CustomEvent('close', {detail: 'stopRecurringDonation'}));
     } 
 }
--- force-app/main/default/lwc/updatePaymentMethodModal/updatePaymentMethodModal.js
+++ force-app/main/default/lwc/updatePaymentMethodModal/updatePaymentMethodModal.js
@@ -1,13 +1,52 @@
 import { LightningElement, api } from 'lwc';
 import updatePaymentMethod from '@salesforce/label/c.updatePaymentMethod';
+
+const ESC_KEY_CODE = 27;
+const ESC_KEY_STRING = "Escape";
+const FOCUSABLE_ELEMENTS = "button";
+const TAB_KEY_CODE = 9;
+const TAB_KEY_STRING = "Tab";
+
 export default class UpdatePaymentMethodModal extends LightningElement {
     @api openUpdatePaymentMethod;
 
     labels = {
-        updatePaymentMethod
+      updatePaymentMethod
+    }   
+
+    renderedCallback() {
+      this.template.addEventListener("keydown", (e) => this.handleKeyUp(e));
+    }
+
+    handleKeyUp(e) {
+        const firstFocusableElement = this._getFocusableElements()[0];
+        const focusableContent = this._getFocusableElements();
+        const lastFocusableElement = focusableContent[focusableContent.length - 1];
+      
+        if (e.shiftKey) {
+          if (this.template.activeElement === firstFocusableElement) {
+            lastFocusableElement.focus();
+            e.preventDefault();
+          }
+        } else if(e.code === ESC_KEY_STRING || e.keyCode === ESC_KEY_CODE) {
+          this.closeModal();
+        } else if(e.code === TAB_KEY_STRING || e.keyCode === TAB_KEY_CODE) {
+          if (this.template.activeElement === lastFocusableElement) {
+            firstFocusableElement.focus();
+            e.preventDefault();
+          }
+        }
+    }   
+
+    _getFocusableElements() {
+      const potentialElems = [
+        ...this.template.querySelectorAll(FOCUSABLE_ELEMENTS),
+      ];
+      return potentialElems;
     }
 
     closeModal() {
-        this.dispatchEvent(new CustomEvent('close', {detail: 'updatePaymentMethod'}));
-    } 
+      this.template.removeEventListener("keydown", (e) => this.handleKeyUp(e));
+      this.dispatchEvent(new CustomEvent('close', {detail: 'updatePaymentMethod'}));
+    }
 }
