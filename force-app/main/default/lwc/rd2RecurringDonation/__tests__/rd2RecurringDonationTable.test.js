f23072854b64e26fb2d7bcc5cf27aea17bd5853f
mcasco <mcasco@salesforce.com>
2022-06-07 10:03:58 -0400

    change jest references

--- force-app/main/default/lwc/rd2RecurringDonation/__tests__/rd2RecurringDonationTable.test.js
+++ force-app/main/default/lwc/rd2RecurringDonation/__tests__/rd2RecurringDonationTable.test.js
@@ -1,6 +1,6 @@
 import { createElement } from 'lwc';
-import recurringDonationTable from '../recurringDonationTable';
-const MOCK_DATA = require('./data/recurringDonationTableData.json');
+import rd2RecurringDonation from '../rd2RecurringDonation';
+const MOCK_DATA = require('./data/rd2RecurringDonationData.json');
 
 // Helper function to wait until the microtask queue is empty. This is needed for promise
 // timing when calling imperative Apex.
@@ -12,7 +12,7 @@ const MOCK_DATA = require('./data/recurringDonationTableData.json');
     });
   
     it('renders table correctly', ()=>{
-      const element = createElement('c-rd2-recurring-donation', { is: recurringDonationTable});
+      const element = createElement('c-rd2-recurring-donation', { is: rd2RecurringDonation});
       element.data = MOCK_DATA;
       element.canBeUpdated = true;
       document.body.appendChild(element);
