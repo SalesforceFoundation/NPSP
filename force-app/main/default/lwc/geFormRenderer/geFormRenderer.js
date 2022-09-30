8b036f8264caf947e354b483f4f7a5ed01081b78
Daniel Fuller <d.fuller@salesforce.com>
2022-09-30 15:35:49 -0400

    remove unused import

--- force-app/main/default/lwc/geFormRenderer/geFormRenderer.js
+++ force-app/main/default/lwc/geFormRenderer/geFormRenderer.js
@@ -96,8 +96,6 @@ import DATA_IMPORT_RECURRING_DONATION_CARD_EXPIRATION_YEAR
     from '@salesforce/schema/DataImport__c.Recurring_Donation_Card_Expiration_Year__c';
 import DATA_IMPORT_RECURRING_DONATION_CARD_LAST_4
     from '@salesforce/schema/DataImport__c.Recurring_Donation_Card_Last_4__c';
-import DATA_IMPORT_RECURRING_DONATION_STATUS
-    from '@salesforce/schema/DataImport__c.Recurring_Donation_Status__c';
 
 import DATA_IMPORT_ADDITIONAL_OBJECT_FIELD from '@salesforce/schema/DataImport__c.Additional_Object_JSON__c'
 import DATA_IMPORT_ACCOUNT1_IMPORTED_FIELD from '@salesforce/schema/DataImport__c.Account1Imported__c';
