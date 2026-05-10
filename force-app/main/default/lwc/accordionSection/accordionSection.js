69602fe0aa45976b6a37b6f28ec4bbce0d7f93f8
Michael Smith <SmithMichael@Salesforce.com>
2026-05-10 14:30:58 -0700

    address issues with announcing setup sidebar items with state (#7323)

--- force-app/main/default/lwc/accordionSection/accordionSection.js
+++ force-app/main/default/lwc/accordionSection/accordionSection.js
@@ -25,6 +25,15 @@ export default class AccordionSection extends LightningElement {
         return this.isOpen ? toggleInstructionsWhenOpen : toggleInstructionsWhenClosed;
     }
 
+    get ariaExpanded() {
+        return this.isOpen ? "true" : "false";
+    }
+
+    get buttonAriaLabel() {
+        const state = this.isOpen ? "expanded" : "collapsed";
+        return `${this.title}, ${state}`;
+    }
+
     get classes() {
         let classes = ["slds-accordion__section"];
 
@@ -47,6 +56,15 @@ export default class AccordionSection extends LightningElement {
 
     handleToggle() {
         this.toggle();
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        setTimeout(() => this.template.querySelector("button")?.focus(), 0);
+    }
+
+    handleClickableKeydown(event) {
+        if (event.key === "Enter" || event.key === " ") {
+            event.preventDefault();
+            this.handleToggle();
+        }
     }
 
     @api
